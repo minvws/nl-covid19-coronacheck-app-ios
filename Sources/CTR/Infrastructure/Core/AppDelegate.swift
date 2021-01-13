86b1271db6824f096776707164e5f88be706d3df
Rool Paap <rool@rool.nl>
2021-01-13 13:22:11 +0100

    [CTR-11] Fixed headers.

--- Sources/CTR/Infrastructure/Core/AppDelegate.swift
+++ Sources/CTR/Infrastructure/Core/AppDelegate.swift
@@ -1,9 +1,9 @@
-//
-//  AppDelegate.swift
-//  CoronaTester
-//
-//  Created by Rool Paap on 13/01/2021.
-//
+/*
+* Copyright (c) 2021 De Staat der Nederlanden, Ministerie van Volksgezondheid, Welzijn en Sport.
+*  Licensed under the EUROPEAN UNION PUBLIC LICENCE v. 1.2
+*
+*  SPDX-License-Identifier: EUPL-1.2
+*/
 
 import UIKit
 
--- Sources/CTR/Interface/Main/MainCoordinator.swift
+++ Sources/CTR/Interface/Main/MainCoordinator.swift
@@ -1,9 +1,9 @@
-//
-//  MainCoordinator.swift
-//  CTR
-//
-//  Created by Rool Paap on 13/01/2021.
-//
+/*
+* Copyright (c) 2021 De Staat der Nederlanden, Ministerie van Volksgezondheid, Welzijn en Sport.
+*  Licensed under the EUROPEAN UNION PUBLIC LICENCE v. 1.2
+*
+*  SPDX-License-Identifier: EUPL-1.2
+*/
 
 import Foundation
 import UIKit
