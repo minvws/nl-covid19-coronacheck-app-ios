42ec7f3d57c8b23ef2f3f52a331a76c3bbaba45b
Rool Paap <rool@rool.nl>
2021-01-13 08:54:40 +0100

    Initial Empty Xcode Project

---
+++ Sources/CTR/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoronaTester
+//
+//  Created by Rool Paap on 13/01/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
