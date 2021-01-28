3c60dc4c82a20676e31fac560a11da091f9cee5a
Rool Paap <rool@rool.nl>
2021-01-28 16:18:18 +0100

    [Crypto] Generate key on init

--- Sources/CTR/Infrastructure/Core/AppCoordinator.swift
+++ Sources/CTR/Infrastructure/Core/AppCoordinator.swift
@@ -56,6 +56,10 @@ class AppCoordinator: Coordinator {
 		// Start the mainCoordinator
 		let mainCoordinator = MainCoordinator(navigationController: navigationController)
 		startChildCoordinator(mainCoordinator)
+
+		// Here for now, load the crypto manager, check the key
+		let manager = CryptoManager()
+		manager.debug()
 	}
 
 	/// flag for updating configuration
