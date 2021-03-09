2e4d386c2af5d65ee93c42b3a720d3cc0a2838b4
Rool Paap <rool@rool.nl>
2021-03-09 08:39:36 +0100

    CTR-61544: unit test for verifier result scene

--- CTRTests/Interface/Verify/Result/VerifiyResultViewControllerTests.swift
+++ CTRTests/Interface/Verify/Result/VerifiyResultViewControllerTests.swift
@@ -9,24 +9,24 @@ import XCTest
 @testable import CTR
 
 class VerifyResultViewControllerTests: XCTestCase {
-
+	
 	// MARK: Subject under test
 	var sut: VerifierResultViewController?
-
+	
 	/// The coordinator spy
 	var verifyCoordinatorDelegateSpy = VerifyCoordinatorDelegateSpy()
-
+	
 	/// The view model
 	var viewModel: VerifierResultViewModel?
-
+	
 	var window = UIWindow()
-
+	
 	// MARK: Test lifecycle
 	override func setUp() {
-
+		
 		super.setUp()
 		verifyCoordinatorDelegateSpy = VerifyCoordinatorDelegateSpy()
-
+		
 		viewModel = VerifierResultViewModel(
 			coordinator: verifyCoordinatorDelegateSpy,
 			attributes: Attributes(
@@ -41,25 +41,25 @@ class VerifyResultViewControllerTests: XCTestCase {
 		sut = VerifierResultViewController(viewModel: viewModel!)
 		window = UIWindow()
 	}
-
+	
 	override func tearDown() {
-
+		
 		super.tearDown()
 	}
-
+	
 	func loadView() {
-
+		
 		if let sut = sut {
 			window.addSubview(sut.view)
 			RunLoop.current.run(until: Date())
 		}
 	}
-
+	
 	// MARK: - Tests
-
+	
 	/// Test all the demo content
 	func testDemo() {
-
+		
 		// Given
 		viewModel?.attributes = Attributes(
 			cryptoAttributes: CrypoAttributes(
@@ -69,10 +69,10 @@ class VerifyResultViewControllerTests: XCTestCase {
 			unixTimeStamp: 0
 		)
 		loadView()
-
+		
 		// When
 		viewModel?.checkAttributes()
-
+		
 		// Then
 		guard let strongSut = sut else {
 			XCTFail("Can't unwrap sut")
@@ -81,12 +81,12 @@ class VerifyResultViewControllerTests: XCTestCase {
 		XCTAssertEqual(strongSut.sceneView.title, .verifierResultDemoTitle, "Title should match")
 		XCTAssertEqual(strongSut.sceneView.message, .verifierResultDemoMessage, "Message should match")
 		XCTAssertEqual(strongSut.sceneView.imageView.image, .access, "Image should match")
-
+		
 	}
-
+	
 	/// Test all the denied content
 	func testDenied() {
-
+		
 		// Given
 		viewModel?.attributes = Attributes(
 			cryptoAttributes: CrypoAttributes(
@@ -96,10 +96,10 @@ class VerifyResultViewControllerTests: XCTestCase {
 			unixTimeStamp: 0
 		)
 		loadView()
-
+		
 		// When
 		viewModel?.checkAttributes()
-
+		
 		// Then
 		guard let strongSut = sut else {
 			XCTFail("Can't unwrap sut")
@@ -108,12 +108,38 @@ class VerifyResultViewControllerTests: XCTestCase {
 		XCTAssertEqual(strongSut.sceneView.title, .verifierResultDeniedTitle, "Title should match")
 		XCTAssertEqual(strongSut.sceneView.message, .verifierResultDeniedMessage, "Message should match")
 		XCTAssertEqual(strongSut.sceneView.imageView.image, .denied, "Image should match")
-
 	}
-
+	
+	/// Test all the denied content
+	func testDenied48hours() {
+		
+		// Given
+		let timeStamp48HoursAgo = Date().timeIntervalSince1970 - (48 * 60 * 60) - 40
+		viewModel?.attributes = Attributes(
+			cryptoAttributes: CrypoAttributes(
+				sampleTime: "\(timeStamp48HoursAgo)",
+				testType: "pcr"
+			),
+			unixTimeStamp: Int64(Date().timeIntervalSince1970)
+		)
+		loadView()
+		
+		// When
+		viewModel?.checkAttributes()
+		
+		// Then
+		guard let strongSut = sut else {
+			XCTFail("Can't unwrap sut")
+			return
+		}
+		XCTAssertEqual(strongSut.sceneView.title, .verifierResultDeniedTitle, "Title should match")
+		XCTAssertEqual(strongSut.sceneView.message, .verifierResultDeniedMessage, "Message should match")
+		XCTAssertEqual(strongSut.sceneView.imageView.image, .denied, "Image should match")
+	}
+	
 	/// Test all the verified content
 	func testVerified() {
-
+		
 		// Given
 		let timeStamp40SecAgo = Date().timeIntervalSince1970 - 40
 		viewModel?.attributes = Attributes(
@@ -123,12 +149,12 @@ class VerifyResultViewControllerTests: XCTestCase {
 			),
 			unixTimeStamp: Int64(timeStamp40SecAgo)
 		)
-
+		
 		loadView()
-
+		
 		// When
 		viewModel?.checkAttributes()
-
+		
 		// Then
 		guard let strongSut = sut else {
 			XCTFail("Can't unwrap sut")
@@ -137,29 +163,29 @@ class VerifyResultViewControllerTests: XCTestCase {
 		XCTAssertEqual(strongSut.sceneView.title, .verifierResultAccessTitle, "Title should match")
 		XCTAssertEqual(strongSut.sceneView.message, .verifierResultAccessMessage, "Message should match")
 		XCTAssertEqual(strongSut.sceneView.imageView.image, .access, "Image should match")
-
+		
 	}
-
+	
 	/// Test the dismiss method
 	func testDismiss() {
-
+		
 		// Given
-
+		
 		// When
 		sut?.closeButtonTapped()
-
+		
 		// Then
 		XCTAssertTrue(verifyCoordinatorDelegateSpy.dismissCalled, "Method should be called")
 	}
-
+	
 	/// Test the link tapped method
 	func testLinkTapped() {
-
+		
 		// Given
-
+		
 		// When
 		sut?.linkTapped()
-
+		
 		// Then
 		XCTAssertTrue(verifyCoordinatorDelegateSpy.presentInformationPageCalled, "Method should be called")
 	}
