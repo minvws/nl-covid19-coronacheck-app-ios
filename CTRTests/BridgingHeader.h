8eb48541c8936e23dff2e50d15c44ffe1163949d
Ian Dundas <contact@iandundas.com>
2021-04-09 15:38:41 +0200

    Add bridging header
    Signed-off-by: Ian Dundas <contact@iandundas.com>

---
+++ CTRTests/BridgingHeader.h
@@ -0,0 +1,18 @@
+//
+/*
+* Copyright (c) 2021 De Staat der Nederlanden, Ministerie van Volksgezondheid, Welzijn en Sport.
+*  Licensed under the EUROPEAN UNION PUBLIC LICENCE v. 1.2
+*
+*  SPDX-License-Identifier: EUPL-1.2
+*/
+
+#ifndef Header_h
+#define Header_h
+
+#import "QCOMockPopoverPresentationController.h"
+#import "UIAlertAction+QCOMock.h"
+#import "UIAlertController+QCOMock.h"
+#import "UIViewController+QCOMock.h"
+
+
+#endif /* Header_h */
