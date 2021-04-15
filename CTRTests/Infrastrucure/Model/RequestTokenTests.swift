1e5babbac3e9b16d85e3ab138f7e96dc4d2dff85
Ian Dundas <contact@iandundas.com>
2021-04-15 11:52:49 +0200

    RequestTokenTests
    Signed-off-by: Ian Dundas <contact@iandundas.com>

---
+++ CTRTests/Infrastrucure/Model/RequestTokenTests.swift
@@ -0,0 +1,70 @@
+//
+/*
+* Copyright (c) 2021 De Staat der Nederlanden, Ministerie van Volksgezondheid, Welzijn en Sport.
+*  Licensed under the EUROPEAN UNION PUBLIC LICENCE v. 1.2
+*
+*  SPDX-License-Identifier: EUPL-1.2
+*/
+
+import Foundation
+import XCTest
+@testable import CTR
+
+class RequestTokenTests: XCTestCase {
+
+    var tokenValidatorSpy: TokenValidatorSpy!
+
+    override func setUp() {
+        super.setUp()
+        tokenValidatorSpy = TokenValidatorSpy()
+    }
+
+    func test_validParameters_areParsedCorrectly() {
+
+        // Arrange
+        let inputProvider = "XXX"
+        let inputToken = "YYYYYYYYYYYY"
+        let inputChecksum = "Z"
+        let inputVersion = "2"
+        let input = "\(inputProvider)-\(inputToken)-\(inputChecksum)\(inputVersion)"
+
+        let expectedVersion = "\(inputVersion).0"
+
+        tokenValidatorSpy.stubbedValidateResult = true
+
+        // Act
+        let sut = RequestToken(input: input, tokenValidator: tokenValidatorSpy)
+
+        // Assert
+
+        XCTAssertNotNil(sut)
+        XCTAssertEqual(sut?.providerIdentifier, inputProvider)
+        XCTAssertEqual(sut?.token, inputToken)
+        XCTAssertEqual(sut?.protocolVersion, expectedVersion)
+
+        // Should also have called the validator:
+        XCTAssert(tokenValidatorSpy.invokedValidate)
+        XCTAssertEqual(tokenValidatorSpy.invokedValidateParameters?.token, input)
+    }
+
+    func test_falselyValidParameters_shouldNotCrash() {
+        // Arrange
+        let input = "XXX-YYY" // not enough components
+
+        tokenValidatorSpy.stubbedValidateResult = true // bug in validator! 🪳
+
+        // Act
+        let sut = RequestToken(input: input, tokenValidator: tokenValidatorSpy)
+
+        // Assert
+        XCTAssertNil(sut) // didn't crash but returned nil.
+    }
+
+    func test_invalidParameters_returnNil() {
+        let tokenValidator = TokenValidator() // use a real one for an integration test here
+        XCTAssertNil(RequestToken(input: "XXX-YYYYYYYYYYYY-ZV", tokenValidator: tokenValidator))
+        XCTAssertNil(RequestToken(input: "XXX-YYYY", tokenValidator: tokenValidator))
+        XCTAssertNil(RequestToken(input: "XXX-YYYYYYYYYYYY-Z", tokenValidator: tokenValidator))
+        XCTAssertNil(RequestToken(input: "XX-YYYYYYYYYYYY-Z2", tokenValidator: tokenValidator))
+    }
+}
