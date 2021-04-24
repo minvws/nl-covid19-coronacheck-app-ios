e3b3570160741954de076f45ca773e8e07f86030
Rool Paap <rool@rool.nl>
2021-04-24 09:23:16 +0200

    CTR-62383: Special build to enable GGD option

--- Sources/CTR/Interface/Holder/ChooseProvider/ChooseProviderViewModel.swift
+++ Sources/CTR/Interface/Holder/ChooseProvider/ChooseProviderViewModel.swift
@@ -86,7 +86,7 @@ class ChooseProviderViewModel: Logging {
 	init(
 		coordinator: HolderCoordinatorDelegate,
 		openIdManager: OpenIdManaging,
-		enableGGD: Bool = false) {
+		enableGGD: Bool = true) {
 
 		self.coordinator = coordinator
 		self.openIdManager = openIdManager
