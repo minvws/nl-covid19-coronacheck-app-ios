b36eecb157274a810b4ec72c1afa378ef2092bcc
Rool Paap <rool@rool.nl>
2021-05-05 20:10:02 +0200

    CTR-62164: Cleanup

--- Sources/CTR/Infrastructure/Services/JailBreakDetector.swift
+++ Sources/CTR/Infrastructure/Services/JailBreakDetector.swift
@@ -12,32 +12,17 @@ protocol JailBreakProtocol {
 
 	/// Is this device jail broken?
 	func isJailBroken() -> Bool
-
-//	/// Should we warn the user
-//	func shouldWarnUser() -> Bool
-//
-//	/// The user has seen the warning
-//	func warningHasBeenSeen()
 }
 
 class JailBreakDetector: JailBreakProtocol, Logging {
-
-//	/// The user settings to store if the warning should be shown / has been seen.
-//	private var userSettings: UserSettingsProtocol
-//
-//	/// Initializer
-//	/// - Parameter userSettings: the user settings
-//	init(userSettings: UserSettingsProtocol = UserSettings()) {
-//
-//		self.userSettings = userSettings
-//	}
 
 	/// Is this device jail broken?
 	func isJailBroken() -> Bool {
 
 		let jailbreakStatus = IOSSecuritySuite.amIJailbrokenWithFailedChecks()
 		if jailbreakStatus.jailbroken {
-			if (jailbreakStatus.failedChecks.contains { $0.check == .existenceOfSuspiciousFiles }) && (jailbreakStatus.failedChecks.contains { $0.check == .suspiciousFilesCanBeOpened }) {
+			if (jailbreakStatus.failedChecks.contains { $0.check == .existenceOfSuspiciousFiles })
+				&& (jailbreakStatus.failedChecks.contains { $0.check == .suspiciousFilesCanBeOpened }) {
 				logInfo("This device is a real jailbroken device")
 				logDebug("Reasons: \(IOSSecuritySuite.amIJailbrokenWithFailMessage())")
 				return true
@@ -45,16 +30,4 @@ class JailBreakDetector: JailBreakProtocol, Logging {
 		}
 		return false
 	}
-
-//	/// Should we warn the user
-//	func shouldWarnUser() -> Bool {
-//
-//		return !userSettings.jailbreakWarningShown
-//	}
-//
-//	/// The user has seen the warning
-//	func warningHasBeenSeen() {
-//
-//		userSettings.jailbreakWarningShown = true
-//	}
 }
