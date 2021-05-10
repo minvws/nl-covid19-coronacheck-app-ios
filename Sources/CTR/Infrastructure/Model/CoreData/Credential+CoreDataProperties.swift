0e8d412358c401e176aa59e06214323958268a5b
Rool Paap <rool@rool.nl>
2021-05-10 12:19:46 +0200

    CC-96: Fixed swiftlint warnings

--- Sources/CTR/Infrastructure/Model/CoreData/Credential+CoreDataProperties.swift
+++ Sources/CTR/Infrastructure/Model/CoreData/Credential+CoreDataProperties.swift
@@ -8,7 +8,6 @@
 import Foundation
 import CoreData
 
-
 extension Credential {
 	
 	@nonobjc public class func fetchRequest() -> NSFetchRequest<Credential> {
--- Sources/CTR/Infrastructure/Model/CoreData/Event+CoreDataProperties.swift
+++ Sources/CTR/Infrastructure/Model/CoreData/Event+CoreDataProperties.swift
@@ -8,7 +8,6 @@
 import Foundation
 import CoreData
 
-
 extension Event {
 	
 	@nonobjc public class func fetchRequest() -> NSFetchRequest<Event> {
