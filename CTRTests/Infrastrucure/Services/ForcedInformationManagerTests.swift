1bca75aba36334aaeab86dda3807c4b34a555463
Glenn Chiu <glenn@glennchiu.com>
2021-05-31 19:32:02 +0200

    Update test
    333

--- CTRTests/Infrastrucure/Services/ForcedInformationManagerTests.swift
+++ CTRTests/Infrastrucure/Services/ForcedInformationManagerTests.swift
@@ -55,9 +55,9 @@ class ForcedInformationManagerTests: XCTestCase {
 		// Given
 		let expectedPage = ForcedInformationPage(
 			image: .onboardingSafely,
-			tagline: .holderUpdatePageTagline,
-			title: .holderUpdatePageTitle,
-			content: .holderUpdatePageContent
+			tagline: .forcedInformationUpdatePageTagline,
+			title: .forcedInformationUpdatePageTitle,
+			content: .forcedInformationUpdatePageContent
 		)
 
 		// When
