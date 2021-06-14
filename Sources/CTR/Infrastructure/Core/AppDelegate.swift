52a055297b6c05031e38b106a16460bf7e87a3a5
Glenn Chiu <glenn@glennchiu.com>
2021-06-14 15:45:39 +0200

    Fix navigation bar glitch and display in full height
    695

--- Sources/CTR/Infrastructure/Core/AppDelegate.swift
+++ Sources/CTR/Infrastructure/Core/AppDelegate.swift
@@ -147,16 +147,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     private func styleUI() {
 
         // Custom navigation bar appearance
-        UINavigationBar.appearance().barTintColor = .clear
         UINavigationBar.appearance().titleTextAttributes = [
             NSAttributedString.Key.foregroundColor: Theme.colors.dark,
             NSAttributedString.Key.font: Theme.fonts.bodyMontserratFixed
         ]
-        UINavigationBar.appearance().isTranslucent = true
+		UINavigationBar.appearance().tintColor = Theme.colors.dark
+		
+		// White navigation bar without bottom separator
+        UINavigationBar.appearance().isTranslucent = false
         UINavigationBar.appearance().shadowImage = UIImage()
         UINavigationBar.appearance().setBackgroundImage(UIImage(), for: .default)
-        UINavigationBar.appearance().backgroundColor = .clear
-
-        UINavigationBar.appearance().tintColor = Theme.colors.dark
+		UINavigationBar.appearance().backgroundColor = Theme.colors.viewControllerBackground
     }
 }
