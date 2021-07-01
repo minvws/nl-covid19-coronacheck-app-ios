6175bf45d6e8e21311b8d966c17d1174a1628a41
Ian Dundas <contact@iandundas.com>
2021-07-01 12:20:00 +0200

    Fix tests
    Signed-off-by: Ian Dundas <contact@iandundas.com>

--- CTRTests/Infrastrucure/Services/RemoteConfigManagerTests.swift
+++ CTRTests/Infrastrucure/Services/RemoteConfigManagerTests.swift
@@ -227,7 +227,6 @@ extension RemoteConfiguration {
 			deactivated: deactivated,
 			informationURL: nil,
 			configTTL: 3600,
-			euLaunchDate: "2021-06-03T14:00:00+00:00",
 			maxValidityHours: 48,
 			recoveryWaitingPeriodDays: 11,
 			requireUpdateBefore: nil,
