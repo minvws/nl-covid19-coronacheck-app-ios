b94db3af73b6d3671598412a702949e083d68a79
Dirk-Willem van Gulik <dirkx@webweaving.org>
2021-07-02 19:22:57 +0200

    Scripts to generate a fake stak to test pinning robustness.

---
+++ Scripts/gen_fake_bananen.sh
@@ -0,0 +1,55 @@
+#!perl
+
+my $ca = 'ca.pem';
+# Fetch the root and resign it
+#
+`curl --silent https://www.identrust.com/node/935 |\
+	openssl pkcs7 -inform DER -print_certs |\
+	openssl x509 |\
+	resign -Ksi > $ca` unless -f $ca;
+
+# Fetch full chain for the bananen API server
+#
+open(STDIN,'false | openssl s_client -connect api-ct.bananenhalen.nl:443 -showcerts |')
+	or die $!;
+
+my $cur = undef;
+my @chain = ();
+while(<STDIN>) {
+	$cur = '' if m/-----BEGIN/;
+	$cur .= $_ if defined($cur);
+	if (m/-----END/) {
+		push @chain, $cur;
+		undef $cur;
+	};
+}
+
+# Replace the private key in each item on the chain from the servery by 
+# one we know; and sign it with the 'higher' up one.
+#
+my $idx = 1000;
+my @filenames = ();
+for my $cert (reverse(@chain)) {
+	open(FH,">$idx.real") or die $!;
+	print FH $cert;
+	close(FH);
+	`resign -K -s -i $idx.real $ca $ca > $idx.pem`;
+	$ca = "$idx.pem";
+	push @filenames, $ca;
+	unlink "$idx.real";
+	$idx++;
+};
+
+$last = pop @filenames;
+$untrusted = join(' ',map { '-untrusted '.$_ } @filenames);
+
+open(FH,">chain.pem") or die $!;
+shift @chain;
+for my $cert (reverse(@chain)) { print FH $cert; };
+close(FH);
+
+# Sanity check on our entire chain; with just the CA.
+#
+$cmd = "openssl verify -CAfile ca.pem -purpose any $untrusted $last";
+system($cmd);
+
---
+++ Scripts/run-fake-server.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+cat 1000.pem 1001.pem > chain.pem                    
+echo
+echo verify that the sever runs with
+echo
+echo "  false | openssl s_client -connect api-ct.bananenhalen.nl:4433 -showcerts -CAfile ca.pem"
+echo
+echo You should see a \"Verify return code: 0 \(ok\)\" near the end.
+echo
+openssl s_server -cert 1002.pem -cert_chain chain.pem
