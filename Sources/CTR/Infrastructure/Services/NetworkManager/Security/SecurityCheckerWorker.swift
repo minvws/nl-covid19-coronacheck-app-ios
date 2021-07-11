e980564018deed7025bef7fe04c4b1ae6ab97761
Rool Paap <rool@rool.nl>
2021-07-11 11:15:39 +0200

    Added some verbose logging

--- Sources/CTR/Infrastructure/Services/NetworkManager/Security/SecurityCheckerWorker.swift
+++ Sources/CTR/Infrastructure/Services/NetworkManager/Security/SecurityCheckerWorker.swift
@@ -47,7 +47,7 @@ class SecurityCheckerWorker: Logging {
 		for certificateAsPemData in trustedCertificates {
 			if let cert = certificateFromPEM(certificateAsPemData: certificateAsPemData) {
 				trustList.append(cert)
-				logVerbose("checkATS: addind cert \(cert.hashValue)")
+				logVerbose("checkATS: adding cert \(cert.hashValue)")
             } else {
                 logError("checkATS: Trust cert conversion failed")
             }
