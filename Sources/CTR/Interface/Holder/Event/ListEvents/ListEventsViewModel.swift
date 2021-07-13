9a04915f36bedb0fcec69b2e11f31a6192876af0
Rool Paap <rool@rool.nl>
2021-07-13 11:33:43 +0200

    Split the list event into two parts

--- Sources/CTR/Interface/Holder/Event/ListEvents/ListEventsViewModel.swift
+++ Sources/CTR/Interface/Holder/Event/ListEvents/ListEventsViewModel.swift
@@ -4,7 +4,6 @@
 *
 *  SPDX-License-Identifier: EUPL-1.2
 */
-// swiftlint:disable type_body_length file_length
 
 import Foundation
 
@@ -15,10 +14,10 @@ class ListEventsViewModel: PreventableScreenCapture, Logging {
 	weak var coordinator: (EventCoordinatorDelegate & OpenUrlProtocol)?
 
 	private var walletManager: WalletManaging
-	private var remoteConfigManager: RemoteConfigManaging
+	internal var remoteConfigManager: RemoteConfigManaging
 	private let greenCardLoader: GreenCardLoading
 
-	private var eventMode: EventMode
+	internal var eventMode: EventMode
 
 	private lazy var progressIndicationCounter: ProgressIndicationCounter = {
 		ProgressIndicationCounter { [weak self] in
@@ -27,34 +26,34 @@ class ListEventsViewModel: PreventableScreenCapture, Logging {
 		}
 	}()
 
-	private lazy var dateFormatter: ISO8601DateFormatter = {
+	internal lazy var dateFormatter: ISO8601DateFormatter = {
 		let dateFormatter = ISO8601DateFormatter()
 		dateFormatter.formatOptions = [.withFullDate]
 		return dateFormatter
 	}()
 	
-	private lazy var printDateFormatter: DateFormatter = {
+	internal lazy var printDateFormatter: DateFormatter = {
 
 		let dateFormatter = DateFormatter()
 		dateFormatter.timeZone = TimeZone(identifier: "Europe/Amsterdam")
 		dateFormatter.dateFormat = "dd MMMM yyyy"
 		return dateFormatter
 	}()
-	private lazy var printTestDateFormatter: DateFormatter = {
+	internal lazy var printTestDateFormatter: DateFormatter = {
 
 		let dateFormatter = DateFormatter()
 		dateFormatter.timeZone = TimeZone(identifier: "Europe/Amsterdam")
 		dateFormatter.dateFormat = "EE d MMMM HH:mm"
 		return dateFormatter
 	}()
-	private lazy var printTestLongDateFormatter: DateFormatter = {
+	internal lazy var printTestLongDateFormatter: DateFormatter = {
 
 		let dateFormatter = DateFormatter()
 		dateFormatter.timeZone = TimeZone(identifier: "Europe/Amsterdam")
 		dateFormatter.dateFormat = "EEEE d MMMM HH:mm"
 		return dateFormatter
 	}()
-	private lazy var printMonthFormatter: DateFormatter = {
+	internal lazy var printMonthFormatter: DateFormatter = {
 
 		let dateFormatter = DateFormatter()
 		dateFormatter.timeZone = TimeZone(identifier: "Europe/Amsterdam")
@@ -157,454 +156,9 @@ class ListEventsViewModel: PreventableScreenCapture, Logging {
 		coordinator?.openUrl(url, inApp: true)
 	}
 
-	// MARK: State Helpers
-
-	private func getViewState(from remoteEvents: [RemoteEvent]) -> ListEventsViewController.State {
-
-		var event30DataSource = [EventDataTuple]()
-
-		// If there is just one pending negative/positive test: Pending State.
-		if remoteEvents.count == 1 &&
-			remoteEvents.first?.wrapper.status == .pending &&
-			(remoteEvents.first?.wrapper.events?.first?.negativeTest != nil || remoteEvents.first?.wrapper.events?.first?.positiveTest != nil) {
-			return pendingEventsState()
-		}
-
-		for eventResponse in remoteEvents {
-			if let identity = eventResponse.wrapper.identity,
-			   let events30 = eventResponse.wrapper.events {
-				for event in events30 {
-					event30DataSource.append(
-						(
-							identity: identity,
-							event: event,
-							providerIdentifier: eventResponse.wrapper.providerIdentifier
-						)
-					)
-				}
-			}
-		}
-
-		if event30DataSource.isEmpty {
-
-			if let event = remoteEvents.first, event.wrapper.protocolVersion == "2.0" {
-				// A test 2.0
-				switch event.wrapper.status {
-					case .complete:
-						if let result = event.wrapper.result, result.negativeResult {
-							return listTest20EventsState(event)
-						}
-					case .pending:
-						return pendingEventsState()
-					default:
-						break
-				}
-			}
-		} else {
-			return listEventsState(event30DataSource, remoteEvents: remoteEvents)
-		}
-
-		return emptyEventsState()
-	}
-
-	// MARK: Empty State
-
-	private func emptyEventsState() -> ListEventsViewController.State {
-
-		switch eventMode {
-			case .recovery:
-				return emptyRecoveryState()
-			case .test:
-				return emptyTestState()
-			case .vaccination:
-				return emptyVaccinationState()
-		}
-	}
-
-	private func emptyVaccinationState() -> ListEventsViewController.State {
-
-		return .emptyEvents(
-			content: ListEventsViewController.Content(
-				title: L.holderVaccinationNolistTitle(),
-				subTitle: L.holderVaccinationNolistMessage(),
-				primaryActionTitle: L.holderVaccinationNolistAction(),
-				primaryAction: { [weak self] in
-					self?.coordinator?.listEventsScreenDidFinish(.stop)
-				},
-				secondaryActionTitle: nil,
-				secondaryAction: nil
-			)
-		)
-	}
-
-	private func emptyTestState() -> ListEventsViewController.State {
-
-		return .emptyEvents(
-			content: ListEventsViewController.Content(
-				title: L.holderTestNolistTitle(),
-				subTitle: L.holderTestNolistMessage(),
-				primaryActionTitle: L.holderTestNolistAction(),
-				primaryAction: { [weak self] in
-					self?.coordinator?.listEventsScreenDidFinish(.stop)
-				},
-				secondaryActionTitle: nil,
-				secondaryAction: nil
-			)
-		)
-	}
-
-	private func emptyRecoveryState() -> ListEventsViewController.State {
-
-		return .emptyEvents(
-			content: ListEventsViewController.Content(
-				title: L.holderRecoveryNolistTitle(),
-				subTitle: L.holderRecoveryNolistMessage(),
-				primaryActionTitle: L.holderRecoveryNolistAction(),
-				primaryAction: { [weak self] in
-					self?.coordinator?.listEventsScreenDidFinish(.stop)
-				},
-				secondaryActionTitle: nil,
-				secondaryAction: nil
-			)
-		)
-	}
-
-	// MARK: List State
-
-	private func listEventsState(
-		_ dataSource: [EventDataTuple],
-		remoteEvents: [RemoteEvent]) -> ListEventsViewController.State {
-
-		var dataSource = dataSource
-		if eventMode == .recovery {
-
-			let recoveryExpirationDays = remoteConfigManager.getConfiguration().recoveryExpirationDays ?? 180
-			let result = filterTooOldRecoveryEvents(dataSource, recoveryEventExpirationDays: recoveryExpirationDays)
-			if result.hasTooOldEvents && result.filteredDataSource.isEmpty {
-				return recoveryEventsTooOld()
-			} else {
-				dataSource = result.filteredDataSource
-			}
-		}
-
-		let rows = getSortedRowsFromEvents(dataSource)
-		guard !rows.isEmpty else {
-			return emptyEventsState()
-		}
-
-		let title: String
-		let subTitle: String
-		switch eventMode {
-			case .vaccination:
-				title = L.holderVaccinationListTitle()
-				subTitle = L.holderVaccinationListMessage()
-			case .recovery:
-				title = L.holderRecoveryListTitle()
-				subTitle = L.holderRecoveryListMessage()
-			case .test:
-				title = L.holderTestresultsResultsTitle()
-				subTitle = L.holderTestresultsResultsText()
-		}
-
-		return .listEvents(
-			content: ListEventsViewController.Content(
-				title: title,
-				subTitle: subTitle,
-				primaryActionTitle: L.holderVaccinationListAction(),
-				primaryAction: { [weak self] in
-					self?.userWantsToMakeQR(remoteEvents: remoteEvents) { [weak self] success in
-						if !success {
-							self?.showEventError(remoteEvents: remoteEvents)
-						}
-					}
-				},
-				secondaryActionTitle: L.holderVaccinationListWrong(),
-				secondaryAction: { [weak self] in
-					self?.coordinator?.listEventsScreenDidFinish(
-						.moreInformation(
-							title: L.holderVaccinationWrongTitle(),
-							body: self?.eventMode == .vaccination ? L.holderVaccinationWrongBody() : L.holderTestresultsWrongBody(),
-							hideBodyForScreenCapture: false
-						)
-					)
-				}
-			),
-			rows: rows
-		)
-	}
-
-	private func getSortedRowsFromEvents(_ dataSource: [EventDataTuple]) -> [ListEventsViewController.Row] {
-
-		let sortedDataSource = dataSource.sorted { lhs, rhs in
-			if let lhsDate = lhs.event.getSortDate(with: dateFormatter),
-			   let rhsDate = rhs.event.getSortDate(with: dateFormatter) {
-				return lhsDate < rhsDate
-			}
-			return false
-		}
-
-		switch eventMode {
-			case .recovery:
-				return getSortedRowsFromRecoveryEvents(sortedDataSource)
-			case .test:
-				return getSortedRowsFromTestEvents(sortedDataSource)
-			case .vaccination:
-				return getSortedRowsFromVaccinationEvents(sortedDataSource)
-		}
-	}
-
-	private func getSortedRowsFromTestEvents(_ sortedDataSource: [EventDataTuple]) -> [ListEventsViewController.Row] {
-
-		var rows = [ListEventsViewController.Row]()
-
-		for dataRow in sortedDataSource {
-
-			let formattedBirthDate: String = dataRow.identity.birthDateString
-				.flatMap(Formatter.getDateFrom)
-				.map(printDateFormatter.string) ?? (dataRow.identity.birthDateString ?? "")
-			let formattedTestDate: String = dataRow.event.negativeTest?.sampleDateString
-				.flatMap(Formatter.getDateFrom)
-				.map(printTestDateFormatter.string) ?? (dataRow.event.negativeTest?.sampleDateString ?? "")
-			let formattedTestLongDate: String = dataRow.event.negativeTest?.sampleDateString
-				.flatMap(Formatter.getDateFrom)
-				.map(printTestLongDateFormatter.string) ?? (dataRow.event.negativeTest?.sampleDateString ?? "")
-
-			rows.append(
-				ListEventsViewController.Row(
-					title: L.holderTestresultsNegative(),
-					subTitle: L.holderEventElementSubtitleTest3(
-						formattedTestDate,
-						dataRow.identity.fullName,
-						formattedBirthDate
-					),
-					action: { [weak self] in
-
-						let testType = self?.remoteConfigManager.getConfiguration().getTestTypeMapping(
-							dataRow.event.negativeTest?.type) ?? (dataRow.event.negativeTest?.type ?? "")
-
-						let manufacturer = self?.remoteConfigManager.getConfiguration().getTestManufacturerMapping(
-							dataRow.event.negativeTest?.manufacturer) ?? (dataRow.event.negativeTest?.manufacturer ?? "")
-
-						self?.coordinator?.listEventsScreenDidFinish(
-							.moreInformation(
-								title: L.holderEventAboutTitle(),
-								body: L.holderEventAboutBodyTest3(
-									dataRow.identity.fullName,
-									formattedBirthDate,
-									testType,
-									dataRow.event.negativeTest?.name ?? "",
-									formattedTestLongDate,
-									L.holderShowqrEuAboutTestNegative(),
-									dataRow.event.negativeTest?.facility ?? "",
-									manufacturer,
-									dataRow.event.unique ?? ""
-								),
-								hideBodyForScreenCapture: true
-							)
-						)
-					}
-				)
-			)
-		}
-		return rows
-	}
-
-	private func getSortedRowsFromVaccinationEvents(_ sortedDataSource: [EventDataTuple]) -> [ListEventsViewController.Row] {
-
-		var rows = [ListEventsViewController.Row]()
-
-		for dataRow in sortedDataSource {
-
-			let formattedBirthDate: String = dataRow.identity.birthDateString
-				.flatMap(Formatter.getDateFrom)
-				.map(printDateFormatter.string) ?? (dataRow.identity.birthDateString ?? "")
-			let formattedShotDate: String = dataRow.event.vaccination?.dateString
-				.flatMap(Formatter.getDateFrom)
-				.map(printDateFormatter.string) ?? (dataRow.event.vaccination?.dateString ?? "")
-			let formattedShotMonth: String = dataRow.event.vaccination?.dateString
-				.flatMap(Formatter.getDateFrom)
-				.map(printMonthFormatter.string) ?? ""
-			let provider: String = remoteConfigManager.getConfiguration().getProviderIdentifierMapping(dataRow.providerIdentifier) ?? ""
-
-			rows.append(
-				ListEventsViewController.Row(
-					title: L.holderVaccinationElementTitle("\(formattedShotMonth) (\(provider))"),
-					subTitle: L.holderVaccinationElementSubtitle(dataRow.identity.fullName, formattedBirthDate),
-					action: { [weak self] in
-
-						var vaccinName = ""
-						if let hpkCode = dataRow.event.vaccination?.hpkCode {
-							vaccinName = self?.remoteConfigManager.getConfiguration().getHpkMapping(hpkCode) ?? ""
-						} else if let brand = dataRow.event.vaccination?.brand {
-							vaccinName = self?.remoteConfigManager.getConfiguration().getBrandMapping(brand) ?? ""
-						}
-
-						let vaccineType = self?.remoteConfigManager.getConfiguration().getTypeMapping(
-							dataRow.event.vaccination?.type) ?? dataRow.event.vaccination?.type ?? ""
-						let vaccineManufacturer = self?.remoteConfigManager.getConfiguration().getVaccinationManufacturerMapping(
-							dataRow.event.vaccination?.manufacturer) ?? dataRow.event.vaccination?.manufacturer ?? ""
-
-						var dosage = ""
-						if let doseNumber = dataRow.event.vaccination?.doseNumber,
-						   let totalDose = dataRow.event.vaccination?.totalDoses {
-							dosage = L.holderVaccinationAboutOff("\(doseNumber)", "\(totalDose)")
-						}
-
-						self?.coordinator?.listEventsScreenDidFinish(
-							.moreInformation(
-								title: L.holderEventAboutTitle(),
-								body: L.holderEventAboutBodyVaccination(
-									dataRow.identity.fullName,
-									formattedBirthDate,
-									vaccinName,
-									vaccineType,
-									vaccineManufacturer,
-									dosage,
-									formattedShotDate,
-									dataRow.event.vaccination?.country ?? "",
-									dataRow.event.unique ?? ""
-								),
-								hideBodyForScreenCapture: true
-							)
-						)
-					}
-				)
-			)
-		}
-
-		return rows
-	}
-
-	private func getSortedRowsFromRecoveryEvents(_ sortedDataSource: [EventDataTuple]) -> [ListEventsViewController.Row] {
-
-		var rows = [ListEventsViewController.Row]()
-		for dataRow in sortedDataSource {
-
-			if dataRow.event.recovery != nil {
-				rows.append(getRowFromRecoveryEvent(dataRow: dataRow))
-			} else if dataRow.event.positiveTest != nil {
-				rows.append(getRowFromPositiveTestEvent(dataRow: dataRow))
-			}
-		}
-		return rows
-	}
-
-	/// Filter out all recovery / positive tests that are older than 180 days (config recoveryEventValidity)
-	/// - Parameter dataSource: the complete data source
-	/// - Returns: the filtered data source
-	private func filterTooOldRecoveryEvents(
-		_ dataSource: [EventDataTuple],
-		recoveryEventExpirationDays: Int) -> (filteredDataSource: [EventDataTuple], hasTooOldEvents: Bool) {
-
-		let now = Date()
-
-		let filteredSource = dataSource.filter { dataRow in
-			if let sampleDate = dataRow.event.positiveTest?.getDate(with: dateFormatter),
-			   let validUntil = Calendar.current.date(byAdding: .day, value: recoveryEventExpirationDays, to: sampleDate) {
-				return validUntil > now
-
-			} else if let validUntilString = dataRow.event.recovery?.validUntil,
-					  let validUntilDate = dateFormatter.date(from: validUntilString) {
-				return validUntilDate > now
-			}
-			return false
-		}
-		return (filteredDataSource: filteredSource, hasTooOldEvents: filteredSource.count != dataSource.count)
-	}
-
-	private func getRowFromRecoveryEvent(dataRow: EventDataTuple) -> ListEventsViewController.Row {
-
-		let formattedBirthDate: String = dataRow.identity.birthDateString
-			.flatMap(Formatter.getDateFrom)
-			.map(printDateFormatter.string) ?? (dataRow.identity.birthDateString ?? "")
-		let formattedTestDate: String = dataRow.event.recovery?.sampleDate
-			.flatMap(Formatter.getDateFrom)
-			.map(printTestDateFormatter.string) ?? (dataRow.event.recovery?.sampleDate ?? "")
-		let formattedShortTestDate: String = dataRow.event.recovery?.sampleDate
-			.flatMap(Formatter.getDateFrom)
-			.map(printDateFormatter.string) ?? (dataRow.event.recovery?.sampleDate ?? "")
-		let formattedShortValidFromDate: String = dataRow.event.recovery?.validFrom
-			.flatMap(Formatter.getDateFrom)
-			.map(printDateFormatter.string) ?? (dataRow.event.recovery?.validFrom ?? "")
-		let formattedShortValidUntilDate: String = dataRow.event.recovery?.validUntil
-			.flatMap(Formatter.getDateFrom)
-			.map(printDateFormatter.string) ?? (dataRow.event.recovery?.validUntil ?? "")
-
-		return ListEventsViewController.Row(
-			title: L.holderTestresultsPositive(),
-			subTitle: L.holderEventElementSubtitleTest3(
-				formattedTestDate,
-				dataRow.identity.fullName,
-				formattedBirthDate
-			),
-			action: { [weak self] in
-				self?.coordinator?.listEventsScreenDidFinish(
-					EventScreenResult.moreInformation(
-						title: L.holderEventAboutTitle(),
-						body: L.holderEventAboutBodyRecovery(
-							dataRow.identity.fullName,
-							formattedBirthDate,
-							formattedShortTestDate,
-							formattedShortValidFromDate,
-							formattedShortValidUntilDate,
-							dataRow.event.unique ?? ""
-						),
-						hideBodyForScreenCapture: true
-					)
-				)
-			}
-		)
-	}
-
-	private func getRowFromPositiveTestEvent(dataRow: EventDataTuple) -> ListEventsViewController.Row {
-
-		let formattedBirthDate: String = dataRow.identity.birthDateString
-			.flatMap(Formatter.getDateFrom)
-			.map(printDateFormatter.string) ?? (dataRow.identity.birthDateString ?? "")
-		let formattedTestDate: String = dataRow.event.positiveTest?.sampleDateString
-			.flatMap(Formatter.getDateFrom)
-			.map(printTestDateFormatter.string) ?? (dataRow.event.positiveTest?.sampleDateString ?? "")
-		let formattedTestLongDate: String = dataRow.event.positiveTest?.sampleDateString
-			.flatMap(Formatter.getDateFrom)
-			.map(printTestLongDateFormatter.string) ?? (dataRow.event.positiveTest?.sampleDateString ?? "")
-
-		return ListEventsViewController.Row(
-			title: L.holderTestresultsPositive(),
-			subTitle: L.holderEventElementSubtitleTest3(
-				formattedTestDate,
-				dataRow.identity.fullName,
-				formattedBirthDate
-			),
-			action: { [weak self] in
-
-				let testType = self?.remoteConfigManager.getConfiguration().getTestTypeMapping(
-					dataRow.event.positiveTest?.type) ?? (dataRow.event.positiveTest?.type ?? "")
-
-				let manufacturer = self?.remoteConfigManager.getConfiguration().getTestManufacturerMapping(
-					dataRow.event.positiveTest?.manufacturer) ?? (dataRow.event.positiveTest?.manufacturer ?? "")
-
-				self?.coordinator?.listEventsScreenDidFinish(
-					.moreInformation(
-						title: L.holderEventAboutTitle(),
-						body: L.holderEventAboutBodyTest3(
-							dataRow.identity.fullName,
-							formattedBirthDate,
-							testType, dataRow.event.positiveTest?.name ?? "",
-							formattedTestLongDate,
-							L.holderShowqrEuAboutTestPostive(),
-							dataRow.event.positiveTest?.facility ?? "",
-							manufacturer, dataRow.event.unique ?? ""
-						),
-						hideBodyForScreenCapture: true
-					)
-				)
-			}
-		)
-	}
-
 	// MARK: Sign the events
 
-	private func userWantsToMakeQR(remoteEvents: [RemoteEvent], completion: @escaping (Bool) -> Void) {
+	internal func userWantsToMakeQR(remoteEvents: [RemoteEvent], completion: @escaping (Bool) -> Void) {
 
 		shouldPrimaryButtonBeEnabled = false
 		progressIndicationCounter.increment()
@@ -678,7 +232,7 @@ class ListEventsViewModel: PreventableScreenCapture, Logging {
 		}
 	}
 
-	private func showEventError(remoteEvents: [RemoteEvent]) {
+	internal func showEventError(remoteEvents: [RemoteEvent]) {
 
 		alert = ListEventsViewController.AlertContent(
 			title: L.generalErrorTitle(),
@@ -813,7 +367,7 @@ class ListEventsViewModel: PreventableScreenCapture, Logging {
 		)
 	}
 
-	private func recoveryEventsTooOld() -> ListEventsViewController.State {
+	internal func recoveryEventsTooOld() -> ListEventsViewController.State {
 
 		return .emptyEvents(
 			content: ListEventsViewController.Content(
