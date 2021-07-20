eb3d257d7ccaa386169777192d4ba106b5b93d45
Ian Dundas <contact@iandundas.com>
2021-07-20 12:53:35 +0200

    Inject date throughout HolderDashboardViewModel deps
    Signed-off-by: Ian Dundas <contact@iandundas.com>

--- Sources/CTR/Interface/Holder/Dashboard/HolderDashboardViewModel.swift
+++ Sources/CTR/Interface/Holder/Dashboard/HolderDashboardViewModel.swift
@@ -86,7 +86,8 @@ final class HolderDashboardViewModel: Logging {
 					self.state.expiredGreenCards.removeAll(where: { $0.id == expiredQR.id })
 				},
 				coordinatorDelegate: coordinator,
-				strippenRefresher: strippenRefresher
+				strippenRefresher: strippenRefresher,
+				now: self.now()
 			)
 			
 			hasAddCertificateMode = state.myQRCards.isEmpty
@@ -109,6 +110,8 @@ final class HolderDashboardViewModel: Logging {
 
 	private let strippenRefresher: DashboardStrippenRefresher
 
+	private let now: () -> Date
+
 	// MARK: -
 
 	/// Initializer
@@ -123,15 +126,17 @@ final class HolderDashboardViewModel: Logging {
 		cryptoManager: CryptoManaging,
 		proofManager: ProofManaging,
 		configuration: ConfigurationGeneralProtocol,
-		dataStoreManager: DataStoreManaging
+		dataStoreManager: DataStoreManaging,
+		now: @escaping () -> Date
 	) {
 
 		self.coordinator = coordinator
 		self.cryptoManager = cryptoManager
 		self.proofManager = proofManager
 		self.configuration = configuration
-		self.datasource = Datasource(dataStoreManager: dataStoreManager)
-
+		self.datasource = Datasource(dataStoreManager: dataStoreManager, walletManager: Services.walletManager, now: { Date() })
+		self.now = now
+		
 		self.strippenRefresher = DashboardStrippenRefresher(
 			minimumThresholdOfValidCredentialDaysRemainingToTriggerRefresh: Services.remoteConfigManager.getConfiguration().credentialRenewalDays ?? 5,
 			walletManager: Services.walletManager,
@@ -266,7 +271,8 @@ final class HolderDashboardViewModel: Logging {
 		state: HolderDashboardViewModel.State,
 		didTapCloseExpiredQR: @escaping (ExpiredQR) -> Void,
 		coordinatorDelegate: (HolderCoordinatorDelegate),
-		strippenRefresher: DashboardStrippenRefresher) -> [HolderDashboardViewController.Card] {
+		strippenRefresher: DashboardStrippenRefresher,
+		now: Date) -> [HolderDashboardViewController.Card] {
 		var cards = [HolderDashboardViewController.Card]()
 
 		if !state.myQRCards.isEmpty {
@@ -301,7 +307,7 @@ final class HolderDashboardViewModel: Logging {
 
 		// for each origin which is in the other region but not in this one, add a new MessageCard to explain.
 		// e.g. "Je vaccinatie is niet geldig in Europa. Je hebt alleen een Nederlandse QR-code."
-		cards += localizedOriginsValidOnlyInOtherRegionsMessages(state: state)
+		cards += localizedOriginsValidOnlyInOtherRegionsMessages(state: state, now: now)
 			.sorted(by: { $0.originType.customSortIndex < $1.originType.customSortIndex })
 			.map { originType, message in
 				return .originNotValidInThisRegion(message: message) {
@@ -410,14 +416,14 @@ extension HolderDashboardViewModel {
 
 // MARK: - Free Functions
 
-private func localizedOriginsValidOnlyInOtherRegionsMessages(state: HolderDashboardViewModel.State) -> [(originType: QRCodeOriginType, message: String)] {
+private func localizedOriginsValidOnlyInOtherRegionsMessages(state: HolderDashboardViewModel.State, now: Date) -> [(originType: QRCodeOriginType, message: String)] {
 
 	// Calculate origins which exist in the other region but are not in this region:
 	let originTypesForCurrentRegion = Set(state.myQRCards
 											.filter { $0.isOfRegion(region: state.qrCodeValidityRegion) }
 											.flatMap { $0.origins }
 											.filter {
-												$0.isNotYetExpired
+												$0.isNotYetExpired(now: now)
 											}
 											.compactMap { $0.type }
 	)
@@ -426,7 +432,7 @@ private func localizedOriginsValidOnlyInOtherRegionsMessages(state: HolderDashbo
 											.filter { !$0.isOfRegion(region: state.qrCodeValidityRegion) }
 											.flatMap { $0.origins }
 											.filter {
-												$0.isNotYetExpired
+												$0.isNotYetExpired(now: now)
 											}
 											.compactMap { $0.type }
 	)
