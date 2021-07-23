540748023540471d4f7c797c5249572f1fc72318
Glenn Chiu <glenn@glennchiu.com>
2021-07-23 16:42:40 +0200

    Add top tab bar

---
+++ Sources/CTR/Interface/Holder/Dashboard/Views/TopTabBar.swift
@@ -0,0 +1,143 @@
+/*
+* Copyright (c) 2021 De Staat der Nederlanden, Ministerie van Volksgezondheid, Welzijn en Sport.
+*  Licensed under the EUROPEAN UNION PUBLIC LICENCE v. 1.2
+*
+*  SPDX-License-Identifier: EUPL-1.2
+*/
+
+import UIKit
+
+final class TopTabBar: BaseView {
+	
+	/// The display constants
+	private enum ViewTraits {
+		
+		enum Margin {
+			static let horizontal: CGFloat = 20
+		}
+		enum Size {
+			static let separatorHeight: CGFloat = 2
+		}
+	}
+	
+	private let domesticButton: TabBarButton = {
+		let button = TabBarButton()
+		button.translatesAutoresizingMaskIntoConstraints = false
+		return button
+	}()
+	
+	private let internationalButton: TabBarButton = {
+		let button = TabBarButton()
+		button.translatesAutoresizingMaskIntoConstraints = false
+		return button
+	}()
+	
+	private let separatorView: UIView = {
+		let view = UIView()
+		view.translatesAutoresizingMaskIntoConstraints = false
+		view.backgroundColor = Theme.colors.grey4
+		return view
+	}()
+	
+	private let stackView: UIStackView = {
+		let stackView = UIStackView()
+		stackView.translatesAutoresizingMaskIntoConstraints = false
+		stackView.axis = .horizontal
+		stackView.distribution = .fillEqually
+		return stackView
+	}()
+	
+	override func setupViews() {
+		super.setupViews()
+		
+		backgroundColor = Theme.colors.viewControllerBackground
+	}
+	
+	override func setupViewHierarchy() {
+		super.setupViewHierarchy()
+		
+		addSubview(stackView)
+		stackView.addArrangedSubview(domesticButton)
+		stackView.addArrangedSubview(internationalButton)
+		
+		stackView.addSubview(separatorView)
+	}
+	
+	override func setupViewConstraints() {
+		super.setupViewConstraints()
+		
+		NSLayoutConstraint.activate([
+			stackView.topAnchor.constraint(equalTo: topAnchor),
+			stackView.leftAnchor.constraint(equalTo: leftAnchor, constant: ViewTraits.Margin.horizontal),
+			stackView.rightAnchor.constraint(equalTo: rightAnchor, constant: -ViewTraits.Margin.horizontal),
+			stackView.bottomAnchor.constraint(equalTo: bottomAnchor),
+			
+			separatorView.leftAnchor.constraint(equalTo: stackView.leftAnchor),
+			separatorView.rightAnchor.constraint(equalTo: stackView.rightAnchor),
+			separatorView.bottomAnchor.constraint(equalTo: stackView.bottomAnchor),
+			separatorView.heightAnchor.constraint(equalToConstant: ViewTraits.Size.separatorHeight)
+		])
+	}
+}
+
+private class TabBarButton: UIControl {
+	
+	/// The display constants
+	private enum ViewTraits {
+		
+		enum Margin {
+			static let top: CGFloat = 24
+			static let bottom: CGFloat = 18
+		}
+	}
+	
+	private let titleLabel: Label = {
+		let label = Label(subhead: nil)
+		label.textAlignment = .center
+		return label
+	}()
+	
+	override init(frame: CGRect) {
+		super.init(frame: frame)
+		
+		setupViews()
+		setupViewHierarchy()
+		setupViewConstraints()
+	}
+	
+	required init?(coder aDecoder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+	
+	/// Setup all the views
+	func setupViews() {
+
+		backgroundColor = Theme.colors.viewControllerBackground
+	}
+
+	/// Setup the view hierarchy
+	func setupViewHierarchy() {
+
+		addSubview(titleLabel)
+	}
+
+	/// Setup all the constraints
+	func setupViewConstraints() {
+
+		NSLayoutConstraint.activate([
+			titleLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
+			titleLabel.centerYAnchor.constraint(equalTo: centerYAnchor),
+			titleLabel.topAnchor.constraint(equalTo: topAnchor, constant: ViewTraits.Margin.top),
+			titleLabel.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -ViewTraits.Margin.bottom),
+			titleLabel.leftAnchor.constraint(greaterThanOrEqualTo: leftAnchor),
+			titleLabel.rightAnchor.constraint(lessThanOrEqualTo: rightAnchor)
+		])
+	}
+	
+	/// The title
+	var title: String? {
+		didSet {
+			titleLabel.text = title
+		}
+	}
+}
