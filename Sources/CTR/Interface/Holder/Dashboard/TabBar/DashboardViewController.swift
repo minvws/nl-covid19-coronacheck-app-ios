5e54b1cb8c45b983e572d0df4d09d31a98b544e3
Glenn Chiu <glenn@glennchiu.com>
2021-07-26 17:09:46 +0200

    Implement paged scrolling and update selection animation

--- Sources/CTR/Interface/Holder/Dashboard/TabBar/DashboardViewController.swift
+++ Sources/CTR/Interface/Holder/Dashboard/TabBar/DashboardViewController.swift
@@ -9,8 +9,33 @@ import UIKit
 
 final class DashboardViewController: BaseViewController {
 	
-	private let topTabBar = TopTabBar()
-	private let scrollView = UIScrollView()
+	private let topTabBar: TopTabBar = {
+		let tabBar = TopTabBar()
+		tabBar.translatesAutoresizingMaskIntoConstraints = false
+		return tabBar
+	}()
+	
+	private let scrollView: UIScrollView = {
+		let scrollView = UIScrollView()
+		scrollView.translatesAutoresizingMaskIntoConstraints = false
+		scrollView.isPagingEnabled = true
+		scrollView.showsHorizontalScrollIndicator = false
+		return scrollView
+	}()
+	
+	private let domesticScrollView: UIScrollView = {
+		let scrollView = UIScrollView()
+		scrollView.translatesAutoresizingMaskIntoConstraints = false
+		scrollView.backgroundColor = Theme.colors.grey5
+		return scrollView
+	}()
+	
+	private let internationalScrollView: UIScrollView = {
+		let scrollView = UIScrollView()
+		scrollView.translatesAutoresizingMaskIntoConstraints = false
+		scrollView.backgroundColor = Theme.colors.grey4
+		return scrollView
+	}()
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
@@ -18,9 +43,10 @@ final class DashboardViewController: BaseViewController {
 		title = "Mijn bewijzen"
 		
 		view.addSubview(topTabBar)
-		topTabBar.translatesAutoresizingMaskIntoConstraints = false
 		view.addSubview(scrollView)
-		scrollView.translatesAutoresizingMaskIntoConstraints = false
+		scrollView.delegate = self
+		scrollView.addSubview(domesticScrollView)
+		scrollView.addSubview(internationalScrollView)
 		
 		NSLayoutConstraint.activate([
 			topTabBar.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
@@ -30,7 +56,40 @@ final class DashboardViewController: BaseViewController {
 			scrollView.topAnchor.constraint(equalTo: topTabBar.bottomAnchor),
 			scrollView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor),
 			scrollView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor),
-			scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+			scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+			
+			domesticScrollView.topAnchor.constraint(equalTo: scrollView.topAnchor),
+			domesticScrollView.leftAnchor.constraint(equalTo: scrollView.leftAnchor),
+			domesticScrollView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
+			domesticScrollView.widthAnchor.constraint(equalTo: view.safeAreaLayoutGuide.widthAnchor),
+			{
+				let constraint = domesticScrollView.heightAnchor.constraint(equalTo: scrollView.heightAnchor)
+				constraint.priority = .defaultLow
+				return constraint
+			}(),
+			
+			internationalScrollView.topAnchor.constraint(equalTo: scrollView.topAnchor),
+			internationalScrollView.rightAnchor.constraint(equalTo: scrollView.rightAnchor),
+			internationalScrollView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
+			internationalScrollView.leftAnchor.constraint(equalTo: domesticScrollView.rightAnchor),
+			internationalScrollView.widthAnchor.constraint(equalTo: view.safeAreaLayoutGuide.widthAnchor),
+			{
+				let constraint = internationalScrollView.heightAnchor.constraint(equalTo: scrollView.heightAnchor)
+				constraint.priority = .defaultLow
+				return constraint
+			}()
 		])
+	}
+}
+
+extension DashboardViewController: UIScrollViewDelegate {
+	
+	func scrollViewDidScroll(_ scrollView: UIScrollView) {
+		guard scrollView.isDragging else { return }
+		let scrollViewWidth = scrollView.frame.width
+		let pageScroll = 1.5 * scrollViewWidth
+		let nextPage = scrollView.contentOffset.x + scrollViewWidth > pageScroll
+		let tab: TopTabBar.Tab = nextPage ? .international : .domestic
+		topTabBar.select(tab: tab, animated: true)
 	}
 }
