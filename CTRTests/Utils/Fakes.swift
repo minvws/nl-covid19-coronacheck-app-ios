e85a906f59ba5447ba6f25fbffaf13f8c9d8cb30
Ian Dundas <contact@iandundas.com>
2021-08-04 16:56:15 +0200

    ShowQRViewTests
    Signed-off-by: Ian Dundas <contact@iandundas.com>

--- CTRTests/Utils/Fakes.swift
+++ CTRTests/Utils/Fakes.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 @testable import CTR
+import UIKit
 
 extension ForcedInformationConsent {
 
@@ -158,3 +159,18 @@ extension EuCredentialAttributes.DigitalCovidCertificate {
 		)
 	}
 }
+
+
+extension UIImage {
+
+	static func withColor(_ color: UIColor, size: CGSize = CGSize(width: 1, height: 1)) -> UIImage {
+		let format = UIGraphicsImageRendererFormat()
+		format.scale = 1
+		let image =  UIGraphicsImageRenderer(size: size, format: format).image { rendererContext in
+			color.setFill()
+			rendererContext.fill(CGRect(origin: .zero, size: size))
+		}
+		return image
+	}
+}
+
