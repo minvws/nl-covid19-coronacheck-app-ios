35594a92811d472f46f4fbdebfd7b5080724ec17
Rool Paap <rool@rool.nl>
2021-08-06 12:40:16 +0200

    Moved data to separate files for SAN tests

--- CTRTests/Infrastrucure/Crypto/Utility/OpenSSLTests+SAN.swift
+++ CTRTests/Infrastrucure/Crypto/Utility/OpenSSLTests+SAN.swift
@@ -22,18 +22,35 @@ class OpenSSLSANTests: XCTestCase {
 
 	// MARK: - Subject Alternative Name
 
-	func test_subjectAlternativeNames_realLeaf() {
+	func test_subjectAlternativeNames_realLeaf() throws {
+
+		// Chain that is identical in subjectKeyIdentifier, issuerIdentifier, etc
+		// to a real one - but fake from the root down.
+		//
+		// See the Scripts directory:
+		//  gen_fake_bananen.sh         - takes real chain and makes a fake one from it.
+		//  gen_fake_cms_signed_json.sh - uses that to sign a bit of json.
+		//  gen_code.pl                 - generates below hardcoded data.
+		//
+		// For the scripts that have generated below.
+		//
+		// File:       : 1002.real
+		// SHA256 (DER): 19:C4:79:A1:D9:E9:BD:B3:D7:38:E8:41:45:70:16:FB:D8:15:C0:6B:71:96:12:F7:00:9A:1A:C7:E1:9B:F3:53
+		// Subject     : CN = api-ct.bananenhalen.nl
+		// Issuer      : C = US, O = Let's Encrypt, CN = R3
+		//
 
 		// Given
-		expect(self.sut).toNot(beNil())
+		let certificateUrl = try XCTUnwrap(testBundle.url(forResource: "certRealLeaf", withExtension: ".pem"))
+		let certificateData = try Data(contentsOf: certificateUrl)
 
 		// When
-		let sans = sut.getSubjectAlternativeDNSNames(OpenSSLData.realLeaf) as? [String]
+		let sans = sut.getSubjectAlternativeDNSNames(certificateData) as? [String]
 
 		// Then
 		expect(sans).to(haveCount(1))
 		expect(sans?.first) == "api-ct.bananenhalen.nl"
-		expect(self.sut.validateSubjectAlternativeDNSName("api-ct.bananenhalen.nl", forCertificateData: OpenSSLData.realLeaf)) == true
+		expect(self.sut.validateSubjectAlternativeDNSName("api-ct.bananenhalen.nl", forCertificateData: certificateData)) == true
 	}
 
 	func test_subjectAlternativeNames_fakeLeaf() throws {
@@ -69,8 +86,6 @@ class OpenSSLSANTests: XCTestCase {
 		// we should allow that to match.
 		expect(self.sut.validateSubjectAlternativeDNSName("fo@bar", forCertificateData: certificateData)) == false
 	}
-
-
 
 	func test_subjectAlternativeNames_certWithSanRightAndCNWrong() throws {
 
