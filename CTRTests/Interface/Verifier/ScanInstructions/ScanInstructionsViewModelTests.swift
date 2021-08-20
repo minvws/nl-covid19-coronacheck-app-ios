2e77263b8f2681043e6887eff85818395b679c06
Ian Dundas <contact@iandundas.com>
2021-08-20 12:25:53 +0200

    Add Scan Instructions tests

---
+++ CTRTests/Interface/Verifier/ScanInstructions/ScanInstructionsViewModelTests.swift
@@ -0,0 +1,67 @@
+/*
+* Copyright (c) 2021 De Staat der Nederlanden, Ministerie van Volksgezondheid, Welzijn en Sport.
+*  Licensed under the EUROPEAN UNION PUBLIC LICENCE v. 1.2
+*
+*  SPDX-License-Identifier: EUPL-1.2
+*/
+
+import Foundation
+import XCTest
+import Nimble
+@testable import CTR
+
+class ScanInstructionsViewModelTests: XCTestCase {
+
+	var sut: ScanInstructionsViewModel!
+	var coordinatorSpy: ScanInstructionsCoordinatorDelegateSpy!
+
+	override func setUp() {
+		super.setUp()
+		coordinatorSpy = ScanInstructionsCoordinatorDelegateSpy()
+	}
+
+	func test_finishScanInstructions_callsCoordinator() {
+
+		// Arrange
+		sut = ScanInstructionsViewModel(coordinator: coordinatorSpy, pages: [])
+
+		// Act
+		expect(self.coordinatorSpy.invokedUserDidCompletePages) == false
+		sut.finishScanInstructions()
+
+		// Assert
+		expect(self.coordinatorSpy.invokedUserDidCompletePages) == true
+	}
+
+	func test_userTappedBackOnFirstPage_callsCoordinator() {
+
+		// Arrange
+		sut = ScanInstructionsViewModel(coordinator: coordinatorSpy, pages: [])
+
+		// Act
+		expect(self.coordinatorSpy.invokedUserDidCancelScanInstructions) == false
+		sut.userTappedBackOnFirstPage()
+
+		// Assert
+		expect(self.coordinatorSpy.invokedUserDidCancelScanInstructions) == true
+	}
+
+	func test_creatingViewController() {
+		// Arrange
+		let pages = [
+			ScanInstructionsPage(
+				title: L.verifierScaninstructionsRedscreennowwhatTitle(),
+				message: L.verifierScaninstructionsRedscreennowwhatMessage(),
+				image: I.newScanInstructions.redScreenNowWhat(),
+				step: .redScreenNowWhat
+			)
+		]
+		sut = ScanInstructionsViewModel(coordinator: coordinatorSpy, pages: pages)
+
+		// Act
+		let viewController = sut.scanInstructionsViewController(forPage: pages[0])
+
+		// Assert
+		viewController.assertImage()
+	}
+}
