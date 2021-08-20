04cbdc0edc6c5c2cfddd5e11fed44d694ac17b00
Rool Paap <rool@rool.nl>
2021-08-20 08:21:17 +0200

    1427: Removed RawRepresentable option.

--- Sources/CTR/Infrastructure/Model/ErrorState.swift
+++ Sources/CTR/Infrastructure/Model/ErrorState.swift
@@ -9,12 +9,12 @@ import Foundation
 
 struct ErrorCode: CustomStringConvertible {
 
-	struct Flow: RawRepresentable {
-		var rawValue: String
+	struct Flow {
+		var value: String
 	}
 
-	struct Step: RawRepresentable {
-		var rawValue: String
+	struct Step {
+		var value: String
 	}
 
 	var flow: String
@@ -24,8 +24,8 @@ struct ErrorCode: CustomStringConvertible {
 	var detailedCode: Int?
 
 	init(flow: Flow, step: Step, provider: String? = nil, errorCode: String, detailedCode: Int? = nil) {
-		self.flow = flow.rawValue
-		self.step = step.rawValue
+		self.flow = flow.value
+		self.step = step.value
 		self.provider = provider
 		self.errorCode = errorCode
 		self.detailedCode = detailedCode
@@ -48,53 +48,53 @@ struct ErrorCode: CustomStringConvertible {
 
 extension ErrorCode.Flow {
 
-	static let onboarding = ErrorCode.Flow(rawValue: "0")
-	static let commercialTest = ErrorCode.Flow(rawValue: "1")
-	static let vaccination = ErrorCode.Flow(rawValue: "2")
-	static let recovery = ErrorCode.Flow(rawValue: "3")
-	static let ggdTest = ErrorCode.Flow(rawValue: "4")
-	static let hkvi = ErrorCode.Flow(rawValue: "5")
+	static let onboarding = ErrorCode.Flow(value: "0")
+	static let commercialTest = ErrorCode.Flow(value: "1")
+	static let vaccination = ErrorCode.Flow(value: "2")
+	static let recovery = ErrorCode.Flow(value: "3")
+	static let ggdTest = ErrorCode.Flow(value: "4")
+	static let hkvi = ErrorCode.Flow(value: "5")
 }
 
 // MARK: ErrorCode.Step (Startup)
 
 extension ErrorCode.Step {
 
-	static let configuration = ErrorCode.Step(rawValue: "10")
-	static let publicKeys = ErrorCode.Step(rawValue: "20")
+	static let configuration = ErrorCode.Step(value: "10")
+	static let publicKeys = ErrorCode.Step(value: "20")
 }
 
 // MARK: ErrorCode.Step (Common between test / vaccination flows)
 
 extension ErrorCode.Step {
 
-	static let providers = ErrorCode.Step(rawValue: "20")
-	static let storingEvents = ErrorCode.Step(rawValue: "60")
-	static let nonce = ErrorCode.Step(rawValue: "70")
-	static let signer = ErrorCode.Step(rawValue: "80")
-	static let storingCredentials = ErrorCode.Step(rawValue: "90")
+	static let providers = ErrorCode.Step(value: "20")
+	static let storingEvents = ErrorCode.Step(value: "60")
+	static let nonce = ErrorCode.Step(value: "70")
+	static let signer = ErrorCode.Step(value: "80")
+	static let storingCredentials = ErrorCode.Step(value: "90")
 }
 
 // MARK: ErrorCode.Step (Commercial Test)
 
 extension ErrorCode.Step {
 
-	static let testResult = ErrorCode.Step(rawValue: "50")
+	static let testResult = ErrorCode.Step(value: "50")
 }
 
 // MARK: ErrorCode.Step (Vaccination / Recovery / GGD Test)
 
 extension ErrorCode.Step {
 
-	static let tvs = ErrorCode.Step(rawValue: "10")
-	static let accessTokens = ErrorCode.Step(rawValue: "30")
-	static let unomi = ErrorCode.Step(rawValue: "40")
-	static let event = ErrorCode.Step(rawValue: "50")
+	static let tvs = ErrorCode.Step(value: "10")
+	static let accessTokens = ErrorCode.Step(value: "30")
+	static let unomi = ErrorCode.Step(value: "40")
+	static let event = ErrorCode.Step(value: "50")
 }
 
 // MARK: ErrorCode.Step (Paper flow)
 
 extension ErrorCode.Step {
 
-	static let coupling = ErrorCode.Step(rawValue: "10")
+	static let coupling = ErrorCode.Step(value: "10")
 }
