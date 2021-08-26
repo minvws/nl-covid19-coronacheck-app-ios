419e51b40b59947779fcce2b579426169ec06fe4
Rool Paap <rool@rool.nl>
2021-08-26 11:09:48 +0200

    1565: Added requestTimedOut to strippen refresher

--- Sources/CTR/Interface/Holder/Dashboard/Logic/HolderDashboardStrippenRefresher.swift
+++ Sources/CTR/Interface/Holder/Dashboard/Logic/HolderDashboardStrippenRefresher.swift
@@ -113,7 +113,7 @@ class DashboardStrippenRefresher: DashboardStrippenRefreshing, Logging {
 					switch error {
 						case let .credentials(serverError), let .preparingIssue(serverError):
 							if case let ServerError.error(_, _, networkError) = serverError {
-								if networkError == .noInternetConnection {
+								if networkError == .noInternetConnection || networkError == .requestTimedOut {
 									state.loadingState = .noInternet
 								} else {
 									state.errorOccurenceCount += 1
