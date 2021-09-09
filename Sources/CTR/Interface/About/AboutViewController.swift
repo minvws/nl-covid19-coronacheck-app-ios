a1c9a7a74515748a90ae51350be88cbcdadbb3ba
Rool Paap <rool@rool.nl>
2021-09-09 10:25:50 +0200

    1637: Added clear data option for testing

--- Sources/CTR/Interface/About/AboutViewController.swift
+++ Sources/CTR/Interface/About/AboutViewController.swift
@@ -45,6 +45,7 @@ class AboutViewController: BaseViewController {
 		viewModel.$message.binding = { [weak self] in self?.sceneView.message = $0 }
 		viewModel.$listHeader.binding = { [weak self] in self?.sceneView.listHeader = $0 }
 		viewModel.$version.binding = { [weak self] in self?.sceneView.version = $0 }
+		viewModel.$alert.binding = { [weak self] in self?.showAlert($0) }
 
 		setupMenuOptions()
 	}
