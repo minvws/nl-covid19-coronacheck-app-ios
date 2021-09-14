b9a4894f2d68b6783f71383839a79b7a0d92f2ac
Glenn Chiu <glenn@glennchiu.com>
2021-09-14 15:16:17 +0200

    Add display titles

--- Sources/CTR/Interface/Holder/DCCQR/Model/DCCQRDetails.swift
+++ Sources/CTR/Interface/Holder/DCCQR/Model/DCCQRDetails.swift
@@ -32,7 +32,19 @@ enum DCCQRDetailsVaccination: DCCQRDetailable {
 	case uniqueIdentifer
 	
 	var displayTitle: String {
-		return ""
+		switch self {
+			case .name: return L.holderShowqrEuAboutVaccinationName()
+			case .dateOfBirth: return L.holderShowqrEuAboutVaccinationDateofbirth()
+			case .pathogen: return L.holderShowqrEuAboutVaccinationPathogen()
+			case .vaccineBrand: return L.holderShowqrEuAboutVaccinationBrand()
+			case .vaccineType: return L.holderShowqrEuAboutVaccinationType()
+			case .vaccineManufacturer: return L.holderShowqrEuAboutVaccinationManufacturer()
+			case .dosage: return L.holderShowqrEuAboutVaccinationDosage()
+			case .date: return L.holderShowqrEuAboutVaccinationDate()
+			case .country: return L.holderShowqrEuAboutVaccinationCountry()
+			case .issuer: return L.holderShowqrEuAboutVaccinationIssuer()
+			case .uniqueIdentifer: return L.holderShowqrEuAboutVaccinationIdentifier()
+		}
 	}
 }
 
@@ -51,7 +63,20 @@ enum DCCQRDetailsTest: DCCQRDetailable {
 	case uniqueIdentifer
 	
 	var displayTitle: String {
-		return ""
+		switch self {
+			case .name: return L.holderShowqrEuAboutTestName()
+			case .dateOfBirth: return L.holderShowqrEuAboutTestDateofbirth()
+			case .pathogen: return L.holderShowqrEuAboutTestPathogen()
+			case .testType: return L.holderShowqrEuAboutTestType()
+			case .testName: return L.holderShowqrEuAboutTestTestname()
+			case .date: return L.holderShowqrEuAboutTestDate()
+			case .result: return L.holderShowqrEuAboutTestResult()
+			case .facility: return L.holderShowqrEuAboutTestFacility()
+			case .manufacturer: return L.holderShowqrEuAboutTestManufacturer()
+			case .country: return L.holderShowqrEuAboutTestCountry()
+			case .issuer: return L.holderShowqrEuAboutTestIssuer()
+			case .uniqueIdentifer: return L.holderShowqrEuAboutTestIdentifier()
+		}
 	}
 }
 
@@ -67,6 +92,16 @@ enum DCCQRDetailsRecovery: DCCQRDetailable {
 	case uniqueIdentifer
 	
 	var displayTitle: String {
-		return ""
+		switch self {
+			case .name: return L.holderShowqrEuAboutRecoveryName()
+			case .dateOfBirth: return L.holderShowqrEuAboutRecoveryDateofbirth()
+			case .pathogen: return L.holderShowqrEuAboutRecoveryPathogen()
+			case .date: return L.holderShowqrEuAboutRecoveryDate()
+			case .country: return L.holderShowqrEuAboutRecoveryCountry()
+			case .issuer: return L.holderShowqrEuAboutRecoveryIssuer()
+			case .validFrom: return L.holderShowqrEuAboutRecoveryValidfrom()
+			case .validUntil: return L.holderShowqrEuAboutRecoveryValiduntil()
+			case .uniqueIdentifer: return L.holderShowqrEuAboutRecoveryIdentifier()
+		}
 	}
 }
