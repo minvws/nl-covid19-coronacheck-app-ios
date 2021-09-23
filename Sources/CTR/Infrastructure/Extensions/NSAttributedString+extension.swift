6f892c002e35ae8f945c2f7a619da0ba64b6f591
Rool Paap <rool@rool.nl>
2021-09-23 10:42:52 +0200

    1675: Copied / adapted the makeFromHtml method from DBCO project.

--- Sources/CTR/Infrastructure/Extensions/NSAttributedString+extension.swift
+++ Sources/CTR/Infrastructure/Extensions/NSAttributedString+extension.swift
@@ -7,6 +7,19 @@
 
 import UIKit
 
+extension String {
+	func components<T>(separatedBy separators: [T]) -> [String] where T: StringProtocol {
+		var result = [self]
+		for separator in separators {
+			result = result
+				.map { $0.components(separatedBy: separator) }
+				.flatMap { $0 }
+				.filter { !$0.isEmpty }
+		}
+		return result
+	}
+}
+
 extension NSAttributedString {
 
 	/// Set the line height
@@ -39,11 +52,11 @@ public extension NSAttributedString {
 	struct HTMLStyle {
 		let font: UIFont
 		let textColor: UIColor
-		let lineHeight: CGFloat?
-		let kern: CGFloat?
-		let paragraphSpacing: CGFloat?
+		let lineHeight: CGFloat
+		let kern: CGFloat
+		let paragraphSpacing: CGFloat
 
-		init(font: UIFont, textColor: UIColor, lineHeight: CGFloat? = 22, kern: CGFloat = -0.41, paragraphSpacing: CGFloat = 8) {
+		init(font: UIFont, textColor: UIColor, lineHeight: CGFloat = 22, kern: CGFloat = -0.41, paragraphSpacing: CGFloat = 8) {
 			self.font = font
 			self.textColor = textColor
 			self.lineHeight = lineHeight
@@ -57,11 +70,192 @@ public extension NSAttributedString {
 
 public extension NSAttributedString {
 
+	static func makeFromHtml(text: String?, style: HTMLStyle) -> NSAttributedString {
+
+		guard !ProcessInfo.processInfo.isTesting else {
+			return NSAttributedString(string: text ?? "")
+		}
+
+		let text = text ?? ""
+		let result = NSMutableAttributedString()
+
+		let segments = splitIntoSegments(text)
+		segments
+			.map { convertSegment($0, style: style) }
+			.forEach { result.append($0) }
+
+		removeTrailingNewlines(in: result)
+
+		return result
+	}
+
+	private static func splitIntoSegments(_ text: String) -> [String] {
+
+		// Split text for any list tags, so they can be styled separately and properly
+		func wrapInListIfNeeded(_ segment: String) -> String {
+			if segment.contains("<li>") {
+				return "<br/><ul>" + segment + "</ul>"
+			} else {
+				return segment
+			}
+		}
+
+		let components = text
+			.components(separatedBy: ["<ul>", "</ul>"])
+			.map(wrapInListIfNeeded)
+
+		return components
+	}
+
+	private static func convertSegment(_ segment: String, style: HTMLStyle) -> NSAttributedString {
+
+		let attributes = createAttributes(style: style)
+		let data: Data = segment.data(using: .unicode) ?? Data(segment.utf8)
+
+		guard let attributedText = try? NSMutableAttributedString(
+				data: data,
+				options: [.documentType: NSAttributedString.DocumentType.html],
+				documentAttributes: nil) else {
+			return NSAttributedString(string: segment)
+		}
+
+		let fullRange = NSRange(location: 0, length: attributedText.length)
+		attributedText.addAttributes(attributes, range: fullRange)
+
+		replaceFonts(in: attributedText, style: style)
+		replaceBullets(in: attributedText, style: style)
+		replaceListParagraphStyle(in: attributedText, style: style)
+
+		return attributedText
+	}
+
+	private static func createAttributes(style: HTMLStyle) -> [Key: Any] {
+
+		let paragraphStyle = NSMutableParagraphStyle()
+		paragraphStyle.alignment = .natural
+		paragraphStyle.paragraphSpacing = style.paragraphSpacing
+		paragraphStyle.minimumLineHeight = style.lineHeight
+
+		let attributes: [Key: Any] = [
+			.foregroundColor: style.textColor,
+			.paragraphStyle: paragraphStyle,
+			.kern: style.kern
+		]
+
+		return attributes
+	}
+
+	private static func createListParagraphStyle(style: HTMLStyle) -> NSParagraphStyle {
+
+		let tabInterval: CGFloat = 20
+		var tabStops = [NSTextTab]()
+		tabStops.append(NSTextTab(textAlignment: .natural, location: 1))
+		for index in 1...12 {
+			tabStops.append(NSTextTab(textAlignment: .natural, location: CGFloat(index) * tabInterval))
+		}
+
+		let listParagraphStyle = NSMutableParagraphStyle()
+		listParagraphStyle.alignment = .natural
+		listParagraphStyle.paragraphSpacing = 8
+		listParagraphStyle.tabStops = tabStops
+		listParagraphStyle.headIndent = tabInterval
+		listParagraphStyle.firstLineHeadIndent = 0
+		listParagraphStyle.minimumLineHeight = style.lineHeight
+
+		return listParagraphStyle
+	}
+
+	private static func replaceFonts(in text: NSMutableAttributedString, style: HTMLStyle) {
+
+		let fullRange = NSRange(location: 0, length: text.length)
+
+		let boldFontDescriptor = style.font.fontDescriptor.withSymbolicTraits(.traitBold)
+		let boldFont = boldFontDescriptor.map { UIFont(descriptor: $0, size: style.font.pointSize) }
+
+		text.enumerateAttribute(.font, in: fullRange, options: []) { value, range, finished in
+			guard let currentFont = value as? UIFont else { return }
+
+			let newFont: UIFont
+
+			if let boldFont = boldFont, currentFont.fontDescriptor.symbolicTraits.contains(.traitBold) {
+				newFont = boldFont
+			} else {
+				newFont = style.font
+			}
+
+			text.removeAttribute(.font, range: range)
+			text.removeAttribute(.foregroundColor, range: range)
+
+			text.addAttribute(.font, value: newFont, range: range)
+			text.addAttribute(.foregroundColor, value: style.textColor, range: range)
+		}
+	}
+
+	private static let listBulletCharacter = "\u{25CF}"
+
+	private static func replaceBullets(in text: NSMutableAttributedString, style: HTMLStyle) {
+
+		let bulletFont = style.font.withSize(10)
+		let bulletAttributes: [NSAttributedString.Key: Any] = [
+			.font: bulletFont,
+			.foregroundColor: Theme.colors.primary,
+			.baselineOffset: (style.font.xHeight - bulletFont.xHeight) / 2
+		]
+
+		let currentText = text.string
+		var searchRange = NSRange(location: 0, length: currentText.count)
+		var foundRange = NSRange()
+		while searchRange.location < currentText.count {
+			searchRange.length = currentText.count - searchRange.location
+			foundRange = (currentText as NSString).range(of: "•", options: [], range: searchRange)
+			if foundRange.location != NSNotFound {
+				searchRange.location = foundRange.location + foundRange.length
+				text.replaceCharacters(in: foundRange, with: NSAttributedString(string: listBulletCharacter, attributes: bulletAttributes))
+			} else {
+				break
+			}
+		}
+	}
+
+	private static func replaceListParagraphStyle(in text: NSMutableAttributedString, style: HTMLStyle) {
+
+		let fullRange = NSRange(location: 0, length: text.length)
+		let listParagraphStyle = createListParagraphStyle(style: style)
+
+		var previousParagraphIsListStart = false
+		text.enumerateAttribute(.paragraphStyle, in: fullRange, options: []) { value, range, finished in
+
+			let searchText = text.string as NSString
+			if searchText.substring(with: range).starts(with: listBulletCharacter) {
+				var startRange = range
+				if range.location > 0 {
+					// adjust the range so the style is set before the line starts so indentations are properly calculated
+					startRange.location -= 1
+					startRange.length += 1
+				}
+				text.removeAttribute(.paragraphStyle, range: startRange)
+				text.addAttribute(.paragraphStyle, value: listParagraphStyle, range: startRange)
+				previousParagraphIsListStart = true
+			} else if previousParagraphIsListStart {
+				text.removeAttribute(.paragraphStyle, range: range)
+				text.addAttribute(.paragraphStyle, value: listParagraphStyle, range: range)
+				previousParagraphIsListStart = false
+			}
+		}
+	}
+
+	private static func removeTrailingNewlines(in text: NSMutableAttributedString) {
+		
+		while text.string.hasSuffix("\n") {
+			let range = NSRange(location: text.string.count - 1, length: 1)
+			text.replaceCharacters(in: range, with: "")
+		}
+	}
+
 	static func makeFromHtml(
 		text: String?,
 		font: UIFont,
 		textColor: UIColor,
-		textAlignment: NSTextAlignment = .left,
 		lineHeight: CGFloat = 22,
 		kern: CGFloat = -0.41,
 		paragraphSpacing: CGFloat = 8) -> NSAttributedString {
@@ -73,7 +267,7 @@ public extension NSAttributedString {
 
 		let text = text ?? ""
 		let paragraphStyle = NSMutableParagraphStyle()
-		paragraphStyle.alignment = textAlignment
+		paragraphStyle.alignment = .natural
 		paragraphStyle.paragraphSpacing = paragraphSpacing
 		paragraphStyle.minimumLineHeight = lineHeight
 
@@ -87,7 +281,7 @@ public extension NSAttributedString {
 			tabStops.append(NSTextTab(textAlignment: .natural, location: CGFloat(index) * tabInterval))
 		}
 
-		listParagraphStyle.alignment = textAlignment
+		listParagraphStyle.alignment = .natural
 		listParagraphStyle.paragraphSpacing = 0
 		listParagraphStyle.tabStops = tabStops
 		listParagraphStyle.headIndent = tabInterval
