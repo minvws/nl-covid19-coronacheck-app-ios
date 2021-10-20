93461b33ef893cc4dc88da91cbabf44877dcd991
Glenn Chiu <glenn@glennchiu.com>
2021-10-20 10:50:31 +0200

    Update snapshots

--- CTRTests/Interface/Verifier/Result/VerifierResultViewControllerTests.swift
+++ CTRTests/Interface/Verifier/Result/VerifierResultViewControllerTests.swift
@@ -63,7 +63,7 @@ class VerifierResultViewControllerTests: XCTestCase {
 		expect(self.sut.sceneView.secondaryTitle) == L.verifierResultAccessReadmore()
 
 		// Snapshot
-		assertSnapshot(matching: sut, as: .image(precision: 0.98))
+		assertSnapshot(matching: sut, as: .image(precision: 0.998))
 	}
 
 	func testDeniedInvalidQR() throws {
@@ -83,7 +83,7 @@ class VerifierResultViewControllerTests: XCTestCase {
 		expect(self.sut.sceneView.secondaryTitle) == L.verifierResultDeniedReadmore()
 
 		// Snapshot
-		assertSnapshot(matching: sut, as: .image(precision: 0.98))
+		assertSnapshot(matching: sut, as: .image(precision: 0.998))
 	}
 
 	func testVerified() throws {
@@ -105,7 +105,7 @@ class VerifierResultViewControllerTests: XCTestCase {
 		expect(self.sut.sceneView.secondaryTitle) == L.verifierResultAccessReadmore()
 
 		// Snapshot
-		assertSnapshot(matching: sut, as: .image(precision: 0.98))
+		assertSnapshot(matching: sut, as: .image(precision: 0.998))
 	}
 
 	func test_dismiss_shouldNavigateToVerifierWelcome() {
