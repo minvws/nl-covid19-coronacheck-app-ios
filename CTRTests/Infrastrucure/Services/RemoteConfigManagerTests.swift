2764d5dc452433776771763f47a5609753c802d2
Rool Paap <rool@rool.nl>
2021-11-01 10:59:18 +0100

    Verifier 2.5.1: Use a calculated hash

--- CTRTests/Infrastrucure/Services/RemoteConfigManagerTests.swift
+++ CTRTests/Infrastrucure/Services/RemoteConfigManagerTests.swift
@@ -302,7 +302,7 @@ class RemoteConfigManagerTests: XCTestCase {
 		firstConfig.configTTL = 3600
 
 		userSettingsSpy.stubbedConfigFetchedTimestamp = now.addingTimeInterval(40 * days * ago).timeIntervalSince1970
-		userSettingsSpy.stubbedConfigFetchedHash = "6e4b55f8cf6c62e907b692b24c3e2d62b14218a38e0cbed92a166fa4788e7ee7"
+		userSettingsSpy.stubbedConfigFetchedHash = firstConfig.hash
 
 		networkSpy.stubbedGetRemoteConfigurationCompletionResult = (Result.success((firstConfig, firstConfig.data, URLResponse())), ())
 
@@ -407,7 +407,7 @@ class RemoteConfigManagerTests: XCTestCase {
 		// Arrange
 		let configuration = RemoteConfiguration.default
 		userSettingsSpy.stubbedConfigFetchedTimestamp = now.addingTimeInterval(20 * days * ago).timeIntervalSince1970
-		userSettingsSpy.stubbedConfigFetchedHash = "8056f56f53d9ba81facba1bda8d3b06836ccca2d71af02492055f9bc677723cd"
+		userSettingsSpy.stubbedConfigFetchedHash = configuration.hash
 		networkSpy.stubbedGetRemoteConfigurationCompletionResult = (Result.success((configuration, configuration.data, URLResponse())), ())
 
 		var reloadObserverReceivedConfiguration: RemoteConfiguration?
@@ -483,5 +483,10 @@ extension RemoteConfiguration {
 
 	var data: Data {
 		return try! JSONEncoder().encode(self) // swiftlint:disable:this force_try
+	}
+
+	var hash: String? {
+		guard let string = String(data: data, encoding: .utf8) else { return nil }
+		return string.sha256
 	}
 }
