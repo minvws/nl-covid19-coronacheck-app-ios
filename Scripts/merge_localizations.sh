0a90616cb87eec891d4f9a21bc919dd3a8f72111
Ian Dundas <contact@iandundas.com>
2021-12-01 13:26:55 +0100

    Update merge_localizations now that real project is known

--- Scripts/merge_localizations.sh
+++ Scripts/merge_localizations.sh
@@ -1,13 +1,13 @@
 
 ## localizations: 
-shortname=( "nl" "en" )
-longname=( "nl-NL" "en-EN" )
+array=( "nl" "en" )
 
-for i in "${!shortname[@]}"; do
-    COMBINED_PATH="Sources/CTR/Infrastructure/Resources/Localization/${shortname[i]}.lproj/Localizable.strings"
+for i in "${array[@]}"
+do
+    COMBINED_PATH="Sources/CTR/Infrastructure/Resources/Localization/${i}.lproj/Localizable.strings"
 
-    cat "Localizations/Holder/${longname[i]}.lproj/Localizable.strings" > $COMBINED_PATH
-    cat "Localizations/Verifier/${longname[i]}.lproj/Localizable.strings" >> $COMBINED_PATH
+    cat Localizations/Holder/$i.lproj/Localizable.strings > $COMBINED_PATH
+    cat Localizations/Verifier/$i.lproj/Localizable.strings >> $COMBINED_PATH
 
     # Count duplicate lines
     DUPLICATE_LINE_COUNT=$(sort $COMBINED_PATH | sed '/\/\*/d' | sed '/^$/d' | uniq -d | wc -l | xargs)
