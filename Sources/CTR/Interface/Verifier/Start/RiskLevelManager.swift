d799b619df92bb105f4da1ca8a3ef52eaaafdc75
Ian Dundas <contact@iandundas.com>
2021-12-04 20:20:41 +0100

    Add RiskLevelManager

---
+++ Sources/CTR/Interface/Verifier/Start/RiskLevelManager.swift
@@ -0,0 +1,59 @@
+/*
+* Copyright (c) 2021 De Staat der Nederlanden, Ministerie van Volksgezondheid, Welzijn en Sport.
+*  Licensed under the EUROPEAN UNION PUBLIC LICENCE v. 1.2
+*
+*  SPDX-License-Identifier: EUPL-1.2
+*/
+
+import Foundation
+
+protocol RiskLevelManaging {
+	func appendObserver(_ observer: @escaping (RiskLevel?) -> Void) -> RiskLevelManager.ObserverToken
+	func removeObserver(token: RiskLevelManager.ObserverToken)
+	
+	init()
+}
+
+final class RiskLevelManager: RiskLevelManaging {
+	typealias ObserverToken = UUID
+	
+	fileprivate(set) var state: RiskLevel? {
+		didSet {
+			notifyObservers()
+		}
+	}
+	private var observers = [ObserverToken: (RiskLevel?) -> Void]()
+	
+	required init() {
+		// Todo: persist the risk level across launches.
+		state = nil
+	}
+	
+	/// Be careful to use weak references to your observers within the closure, and
+	/// to unregister your observer using the returned `ObserverToken`.
+	func appendObserver(_ observer: @escaping (RiskLevel?) -> Void) -> ObserverToken {
+		let newToken = ObserverToken()
+		observers[newToken] = observer
+		return newToken
+	}
+
+	func removeObserver(token: ObserverToken) {
+		observers[token] = nil
+	}
+
+	private func notifyObservers() {
+		observers.values.forEach { callback in
+			callback(state)
+		}
+	}
+}
+
+#if DEBUG
+extension RiskLevelManaging {
+	
+	func set(riskLevel: RiskLevel) {
+		let casted = self as! RiskLevelManager
+		casted.state = riskLevel
+	}
+}
+#endif
