a83eeb516dc9f10134562dd1d91145f431b8f12d
Ian Dundas <contact@iandundas.com>
2022-01-05 11:36:34 +0100

    Replace commented code with fixed tests

--- CTRTests/Interface/Verifier/ScanLog/ScanLogViewModelTests.swift
+++ CTRTests/Interface/Verifier/ScanLog/ScanLogViewModelTests.swift
@@ -20,7 +20,8 @@ class ScanLogViewModelTests: XCTestCase {
 	override func setUp() {
 		super.setUp()
 		environmentSpies = setupEnvironmentSpies()
-
+		environmentSpies.dataStoreManager = DataStoreManager(.inMemory, flavor: .verifier)
+		
 		coordinatorSpy = VerifierCoordinatorDelegateSpy()
 
 		sut = ScanLogViewModel(coordinator: coordinatorSpy)
@@ -80,97 +81,101 @@ class ScanLogViewModelTests: XCTestCase {
 		expect(self.sut.displayEntries.first) == ScanLogDisplayEntry.message(message: L.scan_log_list_no_items())
 	}
 
-//	func test_oneEntry() {
-//
-//		// Given
-//		let scanManager = ScanLogManager(dataStoreManager: DataStoreManager(.inMemory, flavor: .verifier))
-//		Services.use(scanManager)
-//		scanManager.addScanEntry(riskLevel: .low, date: now.addingTimeInterval(24 * minutes * ago))
-//		var config: RemoteConfiguration = .default
-//		config.scanLogStorageSeconds = Int(Date().timeIntervalSince1970 - now.timeIntervalSince1970 + 3600)
-//
-//		// When
-//		sut = ScanLogViewModel(coordinator: coordinatorSpy)
-//
-//		// Then
-//		expect(self.sut.displayEntries).to(haveCount(1))
-//		expect(self.sut.displayEntries.first) == ScanLogDisplayEntry.entry(type: "3G", timeInterval: "16:38 - nu", message: "1 tot 10 bewijzen gescand", warning: nil)
-//	}
-//
-//	func test_twoEntries_sameMode() {
-//
-//		// Given
-//		let scanManager = ScanLogManager(dataStoreManager: DataStoreManager(.inMemory, flavor: .verifier))
-//		Services.use(scanManager)
-//		scanManager.addScanEntry(riskLevel: .low, date: now.addingTimeInterval(24 * minutes * ago))
-//		scanManager.addScanEntry(riskLevel: .low, date: now.addingTimeInterval(22 * minutes * ago))
-//		var config: RemoteConfiguration = .default
-//		config.scanLogStorageSeconds = Int(Date().timeIntervalSince1970 - now.timeIntervalSince1970 + 3600)
-//
-//		// When
-//		sut = ScanLogViewModel(coordinator: coordinatorSpy)
-//
-//		// Then
-//		expect(self.sut.displayEntries).to(haveCount(1))
-//		expect(self.sut.displayEntries.first) == ScanLogDisplayEntry.entry(type: "3G", timeInterval: "16:38 - nu", message: "1 tot 10 bewijzen gescand", warning: nil)
-//	}
-//
-//	func test_twoEntries_differentMode() {
-//
-//		// Given
-//		let scanManager = ScanLogManager(dataStoreManager: DataStoreManager(.inMemory, flavor: .verifier))
-//		Services.use(scanManager)
-//		scanManager.addScanEntry(riskLevel: .low, date: now.addingTimeInterval(24 * minutes * ago))
-//		scanManager.addScanEntry(riskLevel: .high, date: now.addingTimeInterval(22 * minutes * ago))
-//		var config: RemoteConfiguration = .default
-//		config.scanLogStorageSeconds = Int(Date().timeIntervalSince1970 - now.timeIntervalSince1970 + 3600)
-//
-//		// When
-//		sut = ScanLogViewModel(coordinator: coordinatorSpy)
-//
-//		// Then
-//		expect(self.sut.displayEntries).to(haveCount(2))
-//		expect(self.sut.displayEntries[0]) == ScanLogDisplayEntry.entry(type: "2G", timeInterval: "16:40 - nu", message: "1 tot 10 bewijzen gescand", warning: nil)
-//		expect(self.sut.displayEntries[1]) == ScanLogDisplayEntry.entry(type: "3G", timeInterval: "16:38 - 16:38", message: "1 tot 10 bewijzen gescand", warning: nil)
-//	}
+	func test_oneEntry() {
+		
+		// Given
+		let entry: ScanLogEntry! = ScanLogEntryModel.create(mode: ScanLogManager.lowRisk, date: now.addingTimeInterval(24 * minutes * ago), managedContext: environmentSpies.dataStoreManager.managedObjectContext())
+		environmentSpies.scanLogManagerSpy.stubbedGetScanEntriesResult = .success([entry])
+		
+		var config: RemoteConfiguration = .default
+		config.scanLogStorageSeconds = Int(Date().timeIntervalSince1970 - now.timeIntervalSince1970 + 3600)
+		environmentSpies.remoteConfigManagerSpy.stubbedStoredConfiguration = config
 
-//	func test_complexList() {
-//
-//		// Given
-//		let scanManager = ScanLogManager(dataStoreManager: DataStoreManager(.inMemory, flavor: .verifier))
-//		Services.use(scanManager)
-//		scanManager.addScanEntry(riskLevel: .low, date: now.addingTimeInterval(24 * minutes * ago))
-//		scanManager.addScanEntry(riskLevel: .low, date: now.addingTimeInterval(22 * minutes * ago))
-//		scanManager.addScanEntry(riskLevel: .low, date: now.addingTimeInterval(20 * minutes * ago))
-//		scanManager.addScanEntry(riskLevel: .high, date: now.addingTimeInterval(15 * minutes * ago)) // Mode Switch
-//		scanManager.addScanEntry(riskLevel: .high, date: now.addingTimeInterval(14 * minutes * ago))
-//		scanManager.addScanEntry(riskLevel: .high, date: now.addingTimeInterval(12 * minutes * ago))
-//		scanManager.addScanEntry(riskLevel: .high, date: now.addingTimeInterval(14 * minutes * ago)) // Clock reset
-//		scanManager.addScanEntry(riskLevel: .high, date: now.addingTimeInterval(11 * minutes * ago))
-//		scanManager.addScanEntry(riskLevel: .low, date: now.addingTimeInterval(9 * minutes * ago)) // Another mode switch
-//		scanManager.addScanEntry(riskLevel: .low, date: now.addingTimeInterval(8 * minutes * ago))
-//		scanManager.addScanEntry(riskLevel: .low, date: now.addingTimeInterval(7 * minutes * ago))
-//		scanManager.addScanEntry(riskLevel: .low, date: now.addingTimeInterval(6 * minutes * ago))
-//		scanManager.addScanEntry(riskLevel: .low, date: now.addingTimeInterval(5 * minutes * ago))
-//		scanManager.addScanEntry(riskLevel: .low, date: now.addingTimeInterval(4 * minutes * ago))
-//		scanManager.addScanEntry(riskLevel: .low, date: now.addingTimeInterval(3 * minutes * ago))
-//		scanManager.addScanEntry(riskLevel: .low, date: now.addingTimeInterval(2 * minutes * ago))
-//		scanManager.addScanEntry(riskLevel: .low, date: now.addingTimeInterval(1 * minutes * ago))
-//		scanManager.addScanEntry(riskLevel: .low, date: now.addingTimeInterval(0 * minutes * ago))
-//
-//		var config: RemoteConfiguration = .default
-//		config.scanLogStorageSeconds = Int(Date().timeIntervalSince1970 - now.timeIntervalSince1970 + 3600)
-//
-//		// When
-//		sut = ScanLogViewModel(coordinator: coordinatorSpy)
-//
-//		// Then
-//		expect(self.sut.displayEntries).to(haveCount(4))
-//		expect(self.sut.displayEntries[0]) == ScanLogDisplayEntry.entry(type: "3G", timeInterval: "16:53 - nu", message: "10 tot 20 bewijzen gescand", warning: nil)
-//		expect(self.sut.displayEntries[1]) == ScanLogDisplayEntry.entry(type: "2G", timeInterval: "16:48 - 16:51", message: "1 tot 10 bewijzen gescand", warning: L.scan_log_list_clock_skew_detected())
-//		expect(self.sut.displayEntries[2]) == ScanLogDisplayEntry.entry(type: "2G", timeInterval: "16:47 - 16:50", message: "1 tot 10 bewijzen gescand", warning: nil)
-//		expect(self.sut.displayEntries[3]) == ScanLogDisplayEntry.entry(type: "3G", timeInterval: "16:38 - 16:42", message: "1 tot 10 bewijzen gescand", warning: nil)
-//	}
+		// When
+		sut = ScanLogViewModel(coordinator: coordinatorSpy)
+
+		// Then
+		expect(self.sut.displayEntries).to(haveCount(1))
+		expect(self.sut.displayEntries.first) == ScanLogDisplayEntry.entry(type: "3G", timeInterval: "16:38 - nu", message: "1 tot 10 bewijzen gescand", warning: nil)
+	}
+
+	func test_twoEntries_sameMode() {
+
+		// Given
+		let entry1: ScanLogEntry! = ScanLogEntryModel.create(mode: ScanLogManager.lowRisk, date: now.addingTimeInterval(24 * minutes * ago), managedContext: environmentSpies.dataStoreManager.managedObjectContext())
+		let entry2: ScanLogEntry! = ScanLogEntryModel.create(mode: ScanLogManager.lowRisk, date: now.addingTimeInterval(22 * minutes * ago), managedContext: environmentSpies.dataStoreManager.managedObjectContext())
+		environmentSpies.scanLogManagerSpy.stubbedGetScanEntriesResult = .success([entry1, entry2])
+		
+		var config: RemoteConfiguration = .default
+		config.scanLogStorageSeconds = Int(Date().timeIntervalSince1970 - now.timeIntervalSince1970 + 3600)
+		environmentSpies.remoteConfigManagerSpy.stubbedStoredConfiguration = config
+
+		// When
+		sut = ScanLogViewModel(coordinator: coordinatorSpy)
+
+		// Then
+		expect(self.sut.displayEntries).to(haveCount(1))
+		expect(self.sut.displayEntries.first) == ScanLogDisplayEntry.entry(type: "3G", timeInterval: "16:38 - nu", message: "1 tot 10 bewijzen gescand", warning: nil)
+	}
+
+	func test_twoEntries_differentMode() {
+
+		// Given
+		let entry1: ScanLogEntry! = ScanLogEntryModel.create(mode: ScanLogManager.lowRisk, date: now.addingTimeInterval(24 * minutes * ago), managedContext: environmentSpies.dataStoreManager.managedObjectContext())
+		let entry2: ScanLogEntry! = ScanLogEntryModel.create(mode: ScanLogManager.highRisk, date: now.addingTimeInterval(22 * minutes * ago), managedContext: environmentSpies.dataStoreManager.managedObjectContext())
+		environmentSpies.scanLogManagerSpy.stubbedGetScanEntriesResult = .success([entry1, entry2])
+		
+		var config: RemoteConfiguration = .default
+		config.scanLogStorageSeconds = Int(Date().timeIntervalSince1970 - now.timeIntervalSince1970 + 3600)
+		environmentSpies.remoteConfigManagerSpy.stubbedStoredConfiguration = config
+		
+		// When
+		sut = ScanLogViewModel(coordinator: coordinatorSpy)
+
+		// Then
+		expect(self.sut.displayEntries).to(haveCount(2))
+		expect(self.sut.displayEntries[0]) == ScanLogDisplayEntry.entry(type: "2G", timeInterval: "16:40 - nu", message: "1 tot 10 bewijzen gescand", warning: nil)
+		expect(self.sut.displayEntries[1]) == ScanLogDisplayEntry.entry(type: "3G", timeInterval: "16:38 - 16:38", message: "1 tot 10 bewijzen gescand", warning: nil)
+	}
+
+	func test_complexList() {
+
+		// Given
+		environmentSpies.scanLogManagerSpy.stubbedGetScanEntriesResult = .success([
+			ScanLogEntryModel.create(mode: ScanLogManager.lowRisk, date: now.addingTimeInterval(24 * minutes * ago), managedContext: environmentSpies.dataStoreManager.managedObjectContext())!,
+			ScanLogEntryModel.create(mode: ScanLogManager.lowRisk, date: now.addingTimeInterval(22 * minutes * ago), managedContext: environmentSpies.dataStoreManager.managedObjectContext())!,
+			ScanLogEntryModel.create(mode: ScanLogManager.lowRisk, date: now.addingTimeInterval(20 * minutes * ago), managedContext: environmentSpies.dataStoreManager.managedObjectContext())!,
+			ScanLogEntryModel.create(mode: ScanLogManager.highRisk, date: now.addingTimeInterval(15 * minutes * ago), managedContext: environmentSpies.dataStoreManager.managedObjectContext())!, // Mode Switch
+			ScanLogEntryModel.create(mode: ScanLogManager.highRisk, date: now.addingTimeInterval(14 * minutes * ago), managedContext: environmentSpies.dataStoreManager.managedObjectContext())!,
+			ScanLogEntryModel.create(mode: ScanLogManager.highRisk, date: now.addingTimeInterval(12 * minutes * ago), managedContext: environmentSpies.dataStoreManager.managedObjectContext())!,
+			ScanLogEntryModel.create(mode: ScanLogManager.highRisk, date: now.addingTimeInterval(14 * minutes * ago), managedContext: environmentSpies.dataStoreManager.managedObjectContext())!, // Clock reset
+			ScanLogEntryModel.create(mode: ScanLogManager.highRisk, date: now.addingTimeInterval(11 * minutes * ago), managedContext: environmentSpies.dataStoreManager.managedObjectContext())!,
+			ScanLogEntryModel.create(mode: ScanLogManager.lowRisk, date: now.addingTimeInterval(9 * minutes * ago), managedContext: environmentSpies.dataStoreManager.managedObjectContext())!, // Another mode switch
+			ScanLogEntryModel.create(mode: ScanLogManager.lowRisk, date: now.addingTimeInterval(8 * minutes * ago), managedContext: environmentSpies.dataStoreManager.managedObjectContext())!,
+			ScanLogEntryModel.create(mode: ScanLogManager.lowRisk, date: now.addingTimeInterval(7 * minutes * ago), managedContext: environmentSpies.dataStoreManager.managedObjectContext())!,
+			ScanLogEntryModel.create(mode: ScanLogManager.lowRisk, date: now.addingTimeInterval(6 * minutes * ago), managedContext: environmentSpies.dataStoreManager.managedObjectContext())!,
+			ScanLogEntryModel.create(mode: ScanLogManager.lowRisk, date: now.addingTimeInterval(5 * minutes * ago), managedContext: environmentSpies.dataStoreManager.managedObjectContext())!,
+			ScanLogEntryModel.create(mode: ScanLogManager.lowRisk, date: now.addingTimeInterval(4 * minutes * ago), managedContext: environmentSpies.dataStoreManager.managedObjectContext())!,
+			ScanLogEntryModel.create(mode: ScanLogManager.lowRisk, date: now.addingTimeInterval(3 * minutes * ago), managedContext: environmentSpies.dataStoreManager.managedObjectContext())!,
+			ScanLogEntryModel.create(mode: ScanLogManager.lowRisk, date: now.addingTimeInterval(2 * minutes * ago), managedContext: environmentSpies.dataStoreManager.managedObjectContext())!,
+			ScanLogEntryModel.create(mode: ScanLogManager.lowRisk, date: now.addingTimeInterval(1 * minutes * ago), managedContext: environmentSpies.dataStoreManager.managedObjectContext())!,
+			ScanLogEntryModel.create(mode: ScanLogManager.lowRisk, date: now.addingTimeInterval(0 * minutes * ago), managedContext: environmentSpies.dataStoreManager.managedObjectContext())!
+		])
+
+		var config: RemoteConfiguration = .default
+		config.scanLogStorageSeconds = Int(Date().timeIntervalSince1970 - now.timeIntervalSince1970 + 3600)
+		environmentSpies.remoteConfigManagerSpy.stubbedStoredConfiguration = config
+
+		// When
+		sut = ScanLogViewModel(coordinator: coordinatorSpy)
+
+		// Then
+		expect(self.sut.displayEntries).to(haveCount(4))
+		expect(self.sut.displayEntries[0]) == ScanLogDisplayEntry.entry(type: "3G", timeInterval: "16:53 - nu", message: "10 tot 20 bewijzen gescand", warning: nil)
+		expect(self.sut.displayEntries[1]) == ScanLogDisplayEntry.entry(type: "2G", timeInterval: "16:48 - 16:51", message: "1 tot 10 bewijzen gescand", warning: L.scan_log_list_clock_skew_detected())
+		expect(self.sut.displayEntries[2]) == ScanLogDisplayEntry.entry(type: "2G", timeInterval: "16:47 - 16:50", message: "1 tot 10 bewijzen gescand", warning: nil)
+		expect(self.sut.displayEntries[3]) == ScanLogDisplayEntry.entry(type: "3G", timeInterval: "16:38 - 16:42", message: "1 tot 10 bewijzen gescand", warning: nil)
+	}
 
 	func test_firstUseDate_noDate() {
 
