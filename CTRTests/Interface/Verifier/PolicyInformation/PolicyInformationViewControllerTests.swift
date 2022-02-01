f0c43d18970638c181530e65da48e8dd3bc413e3
Glenn Chiu <glenn@glennchiu.com>
2022-02-01 17:15:31 +0100

    Add policy information tests

---
+++ CTRTests/Interface/Verifier/PolicyInformation/PolicyInformationViewControllerTests.swift
@@ -0,0 +1,191 @@
+/*
+* Copyright (c) 2021 De Staat der Nederlanden, Ministerie van Volksgezondheid, Welzijn en Sport.
+*  Licensed under the EUROPEAN UNION PUBLIC LICENCE v. 1.2
+*
+*  SPDX-License-Identifier: EUPL-1.2
+*/
+
+import XCTest
+@testable import CTR
+import Nimble
+
+final class PolicyInformationViewControllerTests: XCTestCase {
+	
+	private var sut: PolicyInformationViewController!
+	
+	private var coordinatorSpy: ScanInstructionsCoordinatorDelegateSpy!
+	private var viewModel: PolicyInformationViewModel!
+	private var environmentSpies: EnvironmentSpies!
+	
+	var window = UIWindow()
+	
+	override func setUp() {
+		super.setUp()
+		environmentSpies = setupEnvironmentSpies()
+		coordinatorSpy = ScanInstructionsCoordinatorDelegateSpy()
+	}
+	
+	func loadView() {
+		
+		window.addSubview(sut.view)
+		RunLoop.current.run(until: Date())
+	}
+	
+	// MARK: - Tests
+	
+	func test_view_whenMultipleVerificationPoliciesAreEnabled() {
+		// Given
+		environmentSpies.featureFlagManagerSpy.stubbedAreMultipleVerificationPoliciesEnabledResult = true
+		environmentSpies.riskLevelManagerSpy.stubbedState = nil
+		sut = PolicyInformationViewController(
+			viewModel: .init(
+				coordinator: coordinatorSpy
+			)
+		)
+		
+		// When
+		loadView()
+		
+		// Then
+		expect(self.sut.sceneView.tagline) == L.new_policy_subtitle()
+		expect(self.sut.sceneView.title) == L.new_in_app_risksetting_title()
+		expect(self.sut.sceneView.content) == L.new_in_app_risksetting_subtitle()
+		
+		// Snapshot
+		sut.assertImage()
+	}
+	
+	func test_view_whenPolicyStateIsSetAndMultiplePoliciesAreEnabled() {
+		// Given
+		environmentSpies.featureFlagManagerSpy.stubbedAreMultipleVerificationPoliciesEnabledResult = true
+		environmentSpies.riskLevelManagerSpy.stubbedState = .policy1G
+		sut = PolicyInformationViewController(
+			viewModel: .init(
+				coordinator: coordinatorSpy
+			)
+		)
+		
+		// When
+		loadView()
+		
+		// Then
+		expect(self.sut.sceneView.tagline) == L.new_policy_subtitle()
+		expect(self.sut.sceneView.title) == L.new_in_app_risksetting_title()
+		expect(self.sut.sceneView.content) == L.new_in_app_risksetting_subtitle()
+		
+		// Snapshot
+		sut.assertImage()
+	}
+	
+	func test_view_whenMultipleVerificationPoliciesAreDisabled() {
+		// Given
+		environmentSpies.featureFlagManagerSpy.stubbedAreMultipleVerificationPoliciesEnabledResult = false
+		environmentSpies.riskLevelManagerSpy.stubbedState = nil
+		sut = PolicyInformationViewController(
+			viewModel: .init(
+				coordinator: coordinatorSpy
+			)
+		)
+		
+		// When
+		loadView()
+		
+		// Then
+		expect(self.sut.sceneView.tagline) == L.new_policy_subtitle()
+		expect(self.sut.sceneView.title) == L.new_policy_1G_title()
+		expect(self.sut.sceneView.content) == L.new_policy_1G_subtitle()
+		
+		// Snapshot
+		sut.assertImage()
+	}
+	
+	func test_view_whenPolicyStateIsSetAndMultiplePoliciesAreDisabled() {
+		// Given
+		environmentSpies.featureFlagManagerSpy.stubbedAreMultipleVerificationPoliciesEnabledResult = false
+		environmentSpies.riskLevelManagerSpy.stubbedState = .policy1G
+		sut = PolicyInformationViewController(
+			viewModel: .init(
+				coordinator: coordinatorSpy
+			)
+		)
+		
+		// When
+		loadView()
+		
+		// Then
+		expect(self.sut.sceneView.tagline) == L.new_policy_subtitle()
+		expect(self.sut.sceneView.title) == L.new_policy_1G_title()
+		expect(self.sut.sceneView.content) == L.new_policy_1G_subtitle()
+		
+		// Snapshot
+		sut.assertImage()
+	}
+	
+	func test_primaryButtonTitle_whenPolicyStateIsNilAndMultiplePoliciesAreEnabled_shouldDisplayNext() {
+		// Given
+		environmentSpies.featureFlagManagerSpy.stubbedAreMultipleVerificationPoliciesEnabledResult = true
+		environmentSpies.riskLevelManagerSpy.stubbedState = nil
+		sut = PolicyInformationViewController(
+			viewModel: .init(
+				coordinator: coordinatorSpy
+			)
+		)
+		
+		// When
+		loadView()
+		
+		// Then
+		expect(self.sut.sceneView.footerButtonView.primaryTitle) == L.generalNext()
+	}
+	
+	func test_primaryButtonTitle_whenPolicyStateIsSetAndMultiplePoliciesAreEnabled_shouldDisplayStartScanning() {
+		// Given
+		environmentSpies.featureFlagManagerSpy.stubbedAreMultipleVerificationPoliciesEnabledResult = true
+		environmentSpies.riskLevelManagerSpy.stubbedState = .policy3G
+		sut = PolicyInformationViewController(
+			viewModel: .init(
+				coordinator: coordinatorSpy
+			)
+		)
+		
+		// When
+		loadView()
+		
+		// Then
+		expect(self.sut.sceneView.footerButtonView.primaryTitle) == L.verifierScaninstructionsButtonStartscanning()
+	}
+	
+	func test_primaryButtonTitle_whenOnlyPolicyStateIsSetAndMultiplePoliciesAreDisabled_shouldDisplayStartScanning() {
+		// Given
+		environmentSpies.featureFlagManagerSpy.stubbedAreMultipleVerificationPoliciesEnabledResult = false
+		environmentSpies.riskLevelManagerSpy.stubbedState = .policy3G
+		sut = PolicyInformationViewController(
+			viewModel: .init(
+				coordinator: coordinatorSpy
+			)
+		)
+		
+		// When
+		loadView()
+		
+		// Then
+		expect(self.sut.sceneView.footerButtonView.primaryTitle) == L.verifierScaninstructionsButtonStartscanning()
+	}
+	
+	func test_primaryButtonTitle_whenPolicyStateIsNilAndMultiplePoliciesAreDisabled_shouldDisplayStartScanning() {
+		// Given
+		environmentSpies.featureFlagManagerSpy.stubbedAreMultipleVerificationPoliciesEnabledResult = false
+		environmentSpies.riskLevelManagerSpy.stubbedState = nil
+		sut = PolicyInformationViewController(
+			viewModel: .init(
+				coordinator: coordinatorSpy
+			)
+		)
+		
+		// When
+		loadView()
+		
+		// Then
+		expect(self.sut.sceneView.footerButtonView.primaryTitle) == L.verifierScaninstructionsButtonStartscanning()
+	}
+}
---
+++ CTRTests/Interface/Verifier/PolicyInformation/PolicyInformationViewModelTests.swift
@@ -0,0 +1,155 @@
+/*
+* Copyright (c) 2021 De Staat der Nederlanden, Ministerie van Volksgezondheid, Welzijn en Sport.
+*  Licensed under the EUROPEAN UNION PUBLIC LICENCE v. 1.2
+*
+*  SPDX-License-Identifier: EUPL-1.2
+*/
+
+import XCTest
+@testable import CTR
+import Nimble
+
+final class PolicyInformationViewModelTests: XCTestCase {
+	
+	private var sut: PolicyInformationViewModel!
+	
+	private var coordinatorSpy: ScanInstructionsCoordinatorDelegateSpy!
+	private var environmentSpies: EnvironmentSpies!
+	
+	override func setUp() {
+		super.setUp()
+		environmentSpies = setupEnvironmentSpies()
+		coordinatorSpy = ScanInstructionsCoordinatorDelegateSpy()
+	}
+	
+	func test_bindings_whenPolicyStateIsNilAndMultiplePoliciesAreEnabled() {
+		// Given
+		environmentSpies.featureFlagManagerSpy.stubbedAreMultipleVerificationPoliciesEnabledResult = true
+		environmentSpies.riskLevelManagerSpy.stubbedState = nil
+		sut = .init(coordinator: coordinatorSpy)
+		
+		// When
+		sut.finish()
+		
+		// Then
+		expect(self.sut.tagline) == L.new_policy_subtitle()
+		expect(self.sut.title) == L.new_in_app_risksetting_title()
+		expect(self.sut.content) == L.new_in_app_risksetting_subtitle()
+		expect(self.sut.primaryButtonTitle) == L.generalNext()
+	}
+	
+	func test_bindings_whenPolicyStateIsSetAndMultiplePoliciesAreEnabled() {
+		// Given
+		environmentSpies.featureFlagManagerSpy.stubbedAreMultipleVerificationPoliciesEnabledResult = true
+		environmentSpies.riskLevelManagerSpy.stubbedState = .policy3G
+		sut = .init(coordinator: coordinatorSpy)
+		
+		// When
+		sut.finish()
+		
+		// Then
+		expect(self.sut.tagline) == L.new_policy_subtitle()
+		expect(self.sut.title) == L.new_in_app_risksetting_title()
+		expect(self.sut.content) == L.new_in_app_risksetting_subtitle()
+		expect(self.sut.primaryButtonTitle) == L.verifierScaninstructionsButtonStartscanning()
+	}
+	
+	func test_bindings_whenPolicyStateIsSetAndMultiplePoliciesAreDisabled() {
+		// Given
+		environmentSpies.featureFlagManagerSpy.stubbedAreMultipleVerificationPoliciesEnabledResult = false
+		environmentSpies.riskLevelManagerSpy.stubbedState = .policy3G
+		sut = .init(coordinator: coordinatorSpy)
+		
+		// When
+		sut.finish()
+		
+		// Then
+		expect(self.sut.tagline) == L.new_policy_subtitle()
+		expect(self.sut.title) == L.new_policy_1G_title()
+		expect(self.sut.content) == L.new_policy_1G_subtitle()
+		expect(self.sut.primaryButtonTitle) == L.verifierScaninstructionsButtonStartscanning()
+	}
+	
+	func test_bindings_whenPolicyStateIsNilAndMultiplePoliciesAreDisabled() {
+		// Given
+		environmentSpies.featureFlagManagerSpy.stubbedAreMultipleVerificationPoliciesEnabledResult = false
+		environmentSpies.riskLevelManagerSpy.stubbedState = nil
+		sut = .init(coordinator: coordinatorSpy)
+		
+		// When
+		sut.finish()
+		
+		// Then
+		expect(self.sut.tagline) == L.new_policy_subtitle()
+		expect(self.sut.title) == L.new_policy_1G_title()
+		expect(self.sut.content) == L.new_policy_1G_subtitle()
+		expect(self.sut.primaryButtonTitle) == L.verifierScaninstructionsButtonStartscanning()
+	}
+	
+	func test_finish_whenPolicyStateIsNilAndMultiplePoliciesAreEnabled_shouldShowRiskSetting() {
+		// Given
+		environmentSpies.featureFlagManagerSpy.stubbedAreMultipleVerificationPoliciesEnabledResult = true
+		environmentSpies.riskLevelManagerSpy.stubbedState = nil
+		sut = .init(coordinator: coordinatorSpy)
+		
+		// When
+		sut.finish()
+		
+		// Then
+		expect(self.coordinatorSpy.invokedUserWishesToSelectRiskSetting) == true
+		expect(self.coordinatorSpy.invokedUserDidCompletePages) == false
+	}
+	
+	func test_finish_whenPolicyStateIsSetAndMultiplePoliciesAreEnabled_shouldInvokeCoordinatorComplete() {
+		// Given
+		environmentSpies.featureFlagManagerSpy.stubbedAreMultipleVerificationPoliciesEnabledResult = true
+		environmentSpies.riskLevelManagerSpy.stubbedState = .policy3G
+		sut = .init(coordinator: coordinatorSpy)
+		
+		// When
+		sut.finish()
+		
+		// Then
+		expect(self.coordinatorSpy.invokedUserWishesToSelectRiskSetting) == false
+		expect(self.coordinatorSpy.invokedUserDidCompletePages) == true
+	}
+	
+	func test_finish_whenPolicyStateIsSetAndMultiplePoliciesAreDisabled_shouldInvokeCoordinatorComplete() {
+		// Given
+		environmentSpies.featureFlagManagerSpy.stubbedAreMultipleVerificationPoliciesEnabledResult = false
+		environmentSpies.riskLevelManagerSpy.stubbedState = .policy3G
+		sut = .init(coordinator: coordinatorSpy)
+		
+		// When
+		sut.finish()
+		
+		// Then
+		expect(self.coordinatorSpy.invokedUserWishesToSelectRiskSetting) == false
+		expect(self.coordinatorSpy.invokedUserDidCompletePages) == true
+	}
+	
+	func test_finish_whenPolicyStateIsNilAndMultiplePoliciesAreDisabled_shouldInvokeCoordinatorComplete() {
+		// Given
+		environmentSpies.featureFlagManagerSpy.stubbedAreMultipleVerificationPoliciesEnabledResult = false
+		environmentSpies.riskLevelManagerSpy.stubbedState = nil
+		sut = .init(coordinator: coordinatorSpy)
+		
+		// When
+		sut.finish()
+		
+		// Then
+		expect(self.coordinatorSpy.invokedUserWishesToSelectRiskSetting) == false
+		expect(self.coordinatorSpy.invokedUserDidCompletePages) == true
+	}
+	
+	func test_finish_shouldSaveToUserDefaults() {
+		// Given
+		sut = .init(coordinator: coordinatorSpy)
+		
+		// When
+		sut.finish()
+		
+		// Then
+		expect(self.environmentSpies.userSettingsSpy.invokedPolicyInformationShown) == true
+	}
+}
