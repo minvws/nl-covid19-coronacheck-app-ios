9ab49555590a16c2ebecd14b93006a6afdde6194
Glenn Chiu <glenn@glennchiu.com>
2022-02-01 17:46:55 +0100

    Clean up view traits

--- Sources/CTR/Interface/Verifier/PolicyInformation/PolicyInformationView.swift
+++ Sources/CTR/Interface/Verifier/PolicyInformation/PolicyInformationView.swift
@@ -11,18 +11,21 @@ final class PolicyInformationView: ScrolledStackView {
 	
 	private enum ViewTraits {
 		
-		// Dimensions
-		static let titleLineHeight: CGFloat = 32
-		static let titleKerning: CGFloat = -0.26
-		static let imageHeightPercentage: CGFloat = 0.38
-		static let taglineLineHeight: CGFloat = 22
-		static let taglineKerning: CGFloat = -0.41
-		static let topMargin: CGFloat = 0
-		static let margin: CGFloat = 20
-		
-		// Margins
-		static let taglineSpacing: CGFloat = 8
-		static let titleSpacing: CGFloat = 24
+		enum Title {
+			static let lineHeight: CGFloat = 32
+			static let kerning: CGFloat = -0.26
+		}
+		enum Tagline {
+			static let lineHeight: CGFloat = 22
+			static let kerning: CGFloat = -0.41
+		}
+		enum Margin {
+			static let edge: CGFloat = 20
+		}
+		enum Spacing {
+			static let tagline: CGFloat = 8
+			static let title: CGFloat = 24
+		}
 	}
 	
 	/// The image view
@@ -75,15 +78,12 @@ final class PolicyInformationView: ScrolledStackView {
 		backgroundColor = Theme.colors.viewControllerBackground
 		
 		// No margins on the horizontal sides to display image full width
-		stackViewInset = UIEdgeInsets(top: ViewTraits.topMargin,
-									  left: 0,
-									  bottom: ViewTraits.margin,
-									  right: 0)
+		stackViewInset = .bottom(ViewTraits.Margin.edge)
 		// Apply side margins for labels
 		bottomStackView.insets(.init(top: 0,
-									 leading: ViewTraits.margin,
+									 leading: ViewTraits.Margin.edge,
 									 bottom: 0,
-									 trailing: ViewTraits.margin))
+									 trailing: ViewTraits.Margin.edge))
 		
 		scrollViewContentOffsetObserver = scrollView.observe(\.contentOffset) { [weak self] scrollView, _ in
 			let translatedOffset = scrollView.translatedBottomScrollOffset
@@ -99,9 +99,9 @@ final class PolicyInformationView: ScrolledStackView {
 		addSubview(footerButtonView)
 
 		bottomStackView.addArrangedSubview(taglineLabel)
-		bottomStackView.setCustomSpacing(ViewTraits.taglineSpacing, after: taglineLabel)
+		bottomStackView.setCustomSpacing(ViewTraits.Spacing.tagline, after: taglineLabel)
 		bottomStackView.addArrangedSubview(titleLabel)
-		bottomStackView.setCustomSpacing(ViewTraits.titleSpacing, after: titleLabel)
+		bottomStackView.setCustomSpacing(ViewTraits.Spacing.title, after: titleLabel)
 		bottomStackView.addArrangedSubview(contentTextView)
 
 		stackView.addArrangedSubview(imageView)
@@ -131,8 +131,8 @@ final class PolicyInformationView: ScrolledStackView {
 	var tagline: String? {
 		didSet {
 			taglineLabel.attributedText = tagline?.setLineHeight(
-				ViewTraits.taglineLineHeight,
-				kerning: ViewTraits.taglineKerning,
+				ViewTraits.Tagline.lineHeight,
+				kerning: ViewTraits.Tagline.kerning,
 				textColor: Theme.colors.primary
 			)
 		}
@@ -141,8 +141,8 @@ final class PolicyInformationView: ScrolledStackView {
 	var title: String? {
 		didSet {
 			titleLabel.attributedText = title?.setLineHeight(
-				ViewTraits.titleLineHeight,
-				kerning: ViewTraits.titleKerning
+				ViewTraits.Title.lineHeight,
+				kerning: ViewTraits.Title.kerning
 			)
 		}
 	}
