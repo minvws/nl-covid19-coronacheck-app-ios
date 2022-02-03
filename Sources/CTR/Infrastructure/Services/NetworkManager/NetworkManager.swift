0a403d8c31144984e70af465b09100e9b97bd6d0
Rool Paap <rool@rool.nl>
2022-02-03 16:50:30 +0100

    Undo enable logging

--- Sources/CTR/Infrastructure/Services/NetworkManager/NetworkManager.swift
+++ Sources/CTR/Infrastructure/Services/NetworkManager/NetworkManager.swift
@@ -255,9 +255,9 @@ class NetworkManager: Logging {
 
 	func logResponse<Object>(_ response: HTTPURLResponse, object: Object?) {
 
-//		if response.statusCode != 200 {
+		if response.statusCode != 200 {
 			logDebug("Finished response to URL \(response.url?.absoluteString ?? "") with status \(response.statusCode)")
-//		}
+		}
 		let headers = response.allHeaderFields.map { header, value in
 			return String("\(header): \(value)")
 		}.joined(separator: "\n")
