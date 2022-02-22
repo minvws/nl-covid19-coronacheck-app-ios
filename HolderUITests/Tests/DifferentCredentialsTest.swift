e3da49d02383930c0b55844c797041e1b13cf23c
Joris De Haes <joris@egeniq.com>
2022-02-22 16:15:22 +0100

    #3736 Replace vaccination with a negative test of different person

---
+++ HolderUITests/Tests/DifferentCredentialsTest.swift
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2021 De Staat der Nederlanden, Ministerie van Volksgezondheid, Welzijn en Sport.
+ *  Licensed under the EUROPEAN UNION PUBLIC LICENCE v. 1.2
+ *
+ *  SPDX-License-Identifier: EUPL-1.2
+ */
+
+class DifferentPersonTest: BaseTest {
+	
+	func test_ExistingVaccinationAndNegativeTestOfDifferentPerson_IsNotReplaced() {
+		let person1 = TestData.vacJ1DifferentFullNameReplaces
+		addVaccinationCertificate(for: person1)
+		addRetrievedCertificateToApp()
+		
+		let person2 = TestData.negPcr
+		addTestCertificateFromGGD(for: person2)
+		addRetrievedCertificateToApp()
+		replaceExistingCertificate(false)
+		
+		assertValidDutchVaccinationCertificate(doses: person1.dose, validUntilOffset: person1.vacUntil)
+		assertValidInternationalVaccinationCertificate(doses: person1.doseIntl)
+	}
+	
+	func test_ExistingVaccinationAndNegativeTestOfDifferentPerson_IsReplaced() {
+		let person1 = TestData.vacJ1DifferentFullNameReplaces
+		addVaccinationCertificate(for: person1)
+		addRetrievedCertificateToApp()
+		
+		let person2 = TestData.negPcr
+		addTestCertificateFromGGD(for: person2)
+		addRetrievedCertificateToApp()
+		replaceExistingCertificate(true)
+		
+		assertValidDutchTestCertificate()
+		assertValidInternationalTestCertificate(testType: .pcr)
+	}
+}
