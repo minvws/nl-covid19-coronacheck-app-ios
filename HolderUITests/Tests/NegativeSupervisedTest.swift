749e8792034cc8e1942da2b5cffade1fdcdf85b6
Joris De Haes <joris@egeniq.com>
2022-03-18 13:24:07 +0100

    #3791 Supervised selftest

---
+++ HolderUITests/Tests/NegativeSupervisedTest.swift
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2021 De Staat der Nederlanden, Ministerie van Volksgezondheid, Welzijn en Sport.
+ *  Licensed under the EUROPEAN UNION PUBLIC LICENCE v. 1.2
+ *
+ *  SPDX-License-Identifier: EUPL-1.2
+ */
+
+class NegativeSupervisedTest: BaseTest {
+	
+	override func setUpWithError() throws {
+		self.disclosureMode = DisclosureMode.mode1GWith3G
+		
+		try super.setUpWithError()
+	}
+	
+	func test_negPcrSupervisedSelftest() {
+		let person = TestData.negPcrSupervisedSelftest
+		addTestCertificateFromGGD(for: person)
+		addRetrievedCertificateToApp()
+		
+		assertValidDutchTestCertificate()
+		assertCertificateIsNotValidInternationally(ofType: .test)
+	}
+	
+	func test_negRatSupervisedSelftest() {
+		let person = TestData.negRatSupervisedSelftest
+		addTestCertificateFromGGD(for: person)
+		addRetrievedCertificateToApp()
+		
+		assertValidDutchTestCertificate()
+		assertCertificateIsNotValidInternationally(ofType: .test)
+	}
+}
