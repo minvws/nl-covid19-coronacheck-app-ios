1a19b4ff1bebf36c2166e6b415de1f063372f501
Joris De Haes <joris@egeniq.com>
2022-04-15 14:46:56 +0200

    Split smoketests

--- HolderUITests/Smoketests/WalletSmoke.swift
+++ HolderUITests/Smoketests/WalletSmoke.swift
@@ -24,21 +24,6 @@ class WalletSmoke: BaseTest {
 		assertWalletItem(ofType: .vaccination, with: vac)
 	}
 	
-	func test_vacP3() {
-		let person = TestData.vacP3
-		addVaccinationCertificate(for: person)
-		
-		let vac0 = storeRetrievedCertificateDetails(atIndex: 0)
-		let vac1 = storeRetrievedCertificateDetails(atIndex: 1)
-		let vac2 = storeRetrievedCertificateDetails(atIndex: 2)
-		addRetrievedCertificateToApp()
-		
-		viewWallet()
-		assertWalletItem(ofType: .vaccination, atIndex: 0, with: vac0)
-		assertWalletItem(ofType: .vaccination, atIndex: 1, with: vac1)
-		assertWalletItem(ofType: .vaccination, atIndex: 2, with: vac2)
-	}
-	
 	func test_posPcr() {
 		let person = TestData.posPcrBeforeP1
 		addRecoveryCertificate(for: person)
@@ -49,54 +34,14 @@ class WalletSmoke: BaseTest {
 		assertWalletItem(ofType: .positive, with: pos)
 	}
 	
-	func test_posPcrBeforeP1() {
-		let person = TestData.posPcrBeforeP1
-		addVaccinationCertificate(for: person, combinedWithPositiveTest: true)
-		
-		let vac = storeRetrievedCertificateDetails(atIndex: 0)
-		let pos = storeRetrievedCertificateDetails(atIndex: 1)
-		addRetrievedCertificateToApp()
-		assertCombinedVaccinationAndRecoveryRetrieval()
-		
-		viewWallet()
-		assertWalletItem(ofType: .positive, with: pos)
-		assertWalletItem(ofType: .vaccination, with: vac)
-	}
-	
-	func test_encodingChinese() {
-		let person = TestData.encodingChinese
-		addVaccinationCertificate(for: person, combinedWithPositiveTest: true)
-		let vac0 = storeRetrievedCertificateDetails(atIndex: 0)
-		let vac1 = storeRetrievedCertificateDetails(atIndex: 1)
-		let pos = storeRetrievedCertificateDetails(atIndex: 2)
-		addRetrievedCertificateToApp()
-		assertCombinedVaccinationAndRecoveryRetrieval()
-		
+	func test_negPcr() {
+		let person = TestData.negPcr
 		addTestCertificateFromGGD(for: person)
 		let neg = storeRetrievedCertificateDetails()
 		addRetrievedCertificateToApp()
 		
 		viewWallet()
-		assertWalletItem(ofType: .vaccination, atIndex: 0, with: vac0)
-		assertWalletItem(ofType: .vaccination, atIndex: 1, with: vac1)
-		assertWalletItem(ofType: .positive, with: pos)
 		assertWalletItem(ofType: .negative, with: neg)
-	}
-	
-	func test_negPcrP1() {
-		let person = TestData.negPcrP1
-		addTestCertificateFromGGD(for: person)
-		let neg = storeRetrievedCertificateDetails()
-		addRetrievedCertificateToApp()
-		
-		addVaccinationCertificate(for: person)
-		let vac = storeRetrievedCertificateDetails()
-		addRetrievedCertificateToApp()
-		assertCertificateIsOnlyValidInternationally()
-		
-		viewWallet()
-		assertWalletItem(ofType: .negative, with: neg)
-		assertWalletItem(ofType: .vaccination, with: vac)
 	}
 	
 	func test_replaceSetup() {
@@ -115,77 +60,12 @@ class WalletSmoke: BaseTest {
 		assertWalletItem(ofType: .vaccination, with: vac)
 	}
 	
-	func test_keepSetup() {
-		let setup = TestData.vacP2DifferentSetupSituation
-		addVaccinationCertificate(for: setup)
-		let vac0 = storeRetrievedCertificateDetails(atIndex: 0)
-		let vac1 = storeRetrievedCertificateDetails(atIndex: 1)
-		addRetrievedCertificateToApp()
-		
-		let person = TestData.vacJ1DifferentEverythingReplaces
-		addVaccinationCertificate(for: person)
-		addRetrievedCertificateToApp()
-		replaceExistingCertificate(false)
-		
-		viewWallet()
-		assertAmountOfWalletItems(ofType: .vaccination, is: 2)
-		assertWalletItem(ofType: .vaccination, atIndex: 0, with: vac0)
-		assertWalletItem(ofType: .vaccination, atIndex: 1, with: vac1)
-	}
-	
 	func test_removeVaccination() {
 		let person = TestData.vacP1
 		addVaccinationCertificate(for: person)
 		addRetrievedCertificateToApp()
 		
 		viewWallet()
-		deleteItemFromWallet()
-		assertNoEventsInWallet()
-		
-		returnFromWalletToOverview()
-		assertNoCertificateRetrieved()
-	}
-	
-	func test_removePositiveTest() {
-		let person = TestData.posPcr
-		addRecoveryCertificate(for: person)
-		addRetrievedCertificateToApp()
-		
-		viewWallet()
-		deleteItemFromWallet()
-		assertNoEventsInWallet()
-		
-		returnFromWalletToOverview()
-		assertNoCertificateRetrieved()
-	}
-	
-	func test_removeNegativeTest() {
-		let person = TestData.negPcr
-		addTestCertificateFromGGD(for: person)
-		addRetrievedCertificateToApp()
-		
-		viewWallet()
-		deleteItemFromWallet()
-		assertNoEventsInWallet()
-		
-		returnFromWalletToOverview()
-		assertNoCertificateRetrieved()
-	}
-	
-	func test_removeSeparateEvents() {
-		let person = TestData.posPcrP1
-		addVaccinationCertificate(for: person, combinedWithPositiveTest: true)
-		addRetrievedCertificateToApp()
-		assertCombinedVaccinationAndRecoveryRetrieval()
-		
-		viewWallet()
-		assertAmountOfWalletItems(ofType: .positive, is: 1)
-		assertAmountOfWalletItems(ofType: .vaccination, is: 1)
-		
-		deleteItemFromWallet()
-		assertAmountOfWalletItems(ofType: .positive, is: 0)
-		assertAmountOfWalletItems(ofType: .vaccination, is: 1)
-		
 		deleteItemFromWallet()
 		assertNoEventsInWallet()
 		
