d6c85b64dcfca06512dcaa9b097021a58ebd5f61
Ian Dundas <contact@iandundas.com>
2022-04-29 13:57:12 +0200

    Duplicated ErrorState to be new generalised ContentView

---
+++ Sources/CTR/Interface/Common/ContentView/ContentViewModel.swift
@@ -0,0 +1,29 @@
+/*
+* Copyright (c) 2021 De Staat der Nederlanden, Ministerie van Volksgezondheid, Welzijn en Sport.
+*  Licensed under the EUROPEAN UNION PUBLIC LICENCE v. 1.2
+*
+*  SPDX-License-Identifier: EUPL-1.2
+*/
+
+import Foundation
+
+final class ContentViewModel: Logging {
+
+	@Bindable private(set) var content: Content
+	
+	@Bindable private(set) var showBackButton: Bool
+
+	private var backbuttonAction: (() -> Void)?
+
+	init(content: Content, backAction: (() -> Void)?) {
+
+		self.content = content
+		self.backbuttonAction = backAction
+		self.showBackButton = backbuttonAction != nil
+	}
+
+	func backButtonTapped() {
+
+		backbuttonAction?()
+	}
+}
