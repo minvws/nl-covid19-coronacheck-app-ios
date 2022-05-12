4ef37d485cce26d37abe2a23a004c7c621d7413b
Rool Paap <rool@rool.nl>
2022-05-12 11:04:26 +0200

    Fixed unit tests

--- CTRTests/Infrastrucure/Model/EventGroupModelTests.swift
+++ CTRTests/Infrastrucure/Model/EventGroupModelTests.swift
@@ -120,7 +120,7 @@ class EventGroupModelTests: XCTestCase {
 		
 		// When
 		let objectId = try XCTUnwrap(event1?.objectID)
-		let result = EventGroupModel.delete(objectId)
+		let result = EventGroupModel.delete(objectId, managedObjectContext: context)
 		
 		// Then
 		expect(result.isSuccess) == true
@@ -160,8 +160,8 @@ class EventGroupModelTests: XCTestCase {
 		
 		// When
 		let objectId = try XCTUnwrap(event1?.objectID)
-		_ = EventGroupModel.delete(objectId)
-		let result = EventGroupModel.delete(objectId)
+		_ = EventGroupModel.delete(objectId, managedObjectContext: context)
+		let result = EventGroupModel.delete(objectId, managedObjectContext: context)
 
 		// Then
 		expect(result.isFailure) == true
