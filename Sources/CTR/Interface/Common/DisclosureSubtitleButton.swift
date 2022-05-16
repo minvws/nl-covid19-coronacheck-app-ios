e31d78a6bba961b9cf5d8893624f6d2f03a3008b
Rool Paap <rool@rool.nl>
2022-05-16 08:59:09 +0200

    4357: Better fix for disclosure buttons

--- Sources/CTR/Interface/Common/DisclosureSubtitleButton.swift
+++ Sources/CTR/Interface/Common/DisclosureSubtitleButton.swift
@@ -1,31 +1,33 @@
 /*
-* Copyright (c) 2021 De Staat der Nederlanden, Ministerie van Volksgezondheid, Welzijn en Sport.
-*  Licensed under the EUROPEAN UNION PUBLIC LICENCE v. 1.2
-*
-*  SPDX-License-Identifier: EUPL-1.2
-*/
+ * Copyright (c) 2021 De Staat der Nederlanden, Ministerie van Volksgezondheid, Welzijn en Sport.
+ *  Licensed under the EUROPEAN UNION PUBLIC LICENCE v. 1.2
+ *
+ *  SPDX-License-Identifier: EUPL-1.2
+ */
 
 import UIKit
 
 /// A grey full width button with a title, sub title and a disclosure icon
 class DisclosureSubtitleButton: BaseView {
-
+	
 	/// The display constants
 	private struct ViewTraits {
-
+		
 		// Dimensions
 		static let cornerRadius: CGFloat = 9
-		static let shadowRadius: CGFloat = 6
-		static let shadowOpacity: Float = 0.2
-		static let disclosureHeight: CGFloat = 12
-
+		
 		// Margins
 		static let textMargin: CGFloat = 4.0
-		static let disclosureMargin: CGFloat = 18.0
 		static let topMargin: CGFloat = 13.0
 		static let bottomMargin: CGFloat = 16.0
 		static let leadingMargin: CGFloat = 16.0
-		static let trailingMargin: CGFloat = 12.0
+		
+		enum Disclosure {
+			static let height: CGFloat = 12
+			static let width: CGFloat = 12
+			static let margin: CGFloat = 18.0
+			static let trailingMargin: CGFloat = 8.0
+		}
 		
 		enum Title {
 			static let lineHeight: CGFloat = 22
@@ -36,45 +38,45 @@ class DisclosureSubtitleButton: BaseView {
 			static let kerning: CGFloat = -0.24
 		}
 	}
-
+	
 	/// The title label
 	let titleLabel: Label = {
-
+		
 		return Label(calloutSemiBold: nil).multiline()
 	}()
-
+	
 	/// The sub title label
 	let subtitleLabel: Label = {
-
+		
 		return Label(subhead: nil).multiline()
 	}()
-
+	
 	/// The disclosure image
 	let disclosureView: UIImageView = {
-
+		
 		let view = UIImageView(image: I.disclosure())
 		view.translatesAutoresizingMaskIntoConstraints = false
 		return view
 	}()
-
+	
 	let button: UIButton = {
-
+		
 		let button = UIButton()
 		button.translatesAutoresizingMaskIntoConstraints = false
 		return button
 	}()
-
+	
 	override func setupViews() {
-
+		
 		super.setupViews()
 		backgroundColor = C.primaryBlue5()
 		layer.cornerRadius = ViewTraits.cornerRadius
 		button.addTarget(self, action: #selector(primaryButtonTapped), for: .touchUpInside)
 	}
-
+	
 	/// Setup the hierarchy
 	override func setupViewHierarchy() {
-
+		
 		super.setupViewHierarchy()
 		addSubview(disclosureView)
 		addSubview(titleLabel)
@@ -82,14 +84,14 @@ class DisclosureSubtitleButton: BaseView {
 		button.embed(in: self)
 		bringSubviewToFront(button)
 	}
-
+	
 	/// Setup the constraints
 	override func setupViewConstraints() {
-
+		
 		super.setupViewConstraints()
-
+		
 		NSLayoutConstraint.activate([
-
+			
 			// Title
 			titleLabel.topAnchor.constraint(
 				equalTo: topAnchor,
@@ -101,13 +103,13 @@ class DisclosureSubtitleButton: BaseView {
 			),
 			titleLabel.trailingAnchor.constraint(
 				lessThanOrEqualTo: disclosureView.leadingAnchor,
-				constant: -ViewTraits.trailingMargin
+				constant: -ViewTraits.Disclosure.trailingMargin
 			),
 			titleLabel.bottomAnchor.constraint(
 				equalTo: subtitleLabel.topAnchor,
 				constant: -ViewTraits.textMargin
 			),
-
+			
 			// Message
 			subtitleLabel.leadingAnchor.constraint(
 				equalTo: leadingAnchor,
@@ -118,65 +120,70 @@ class DisclosureSubtitleButton: BaseView {
 				constant: -ViewTraits.bottomMargin
 			),
 			subtitleLabel.trailingAnchor.constraint(
-				equalTo: disclosureView.leadingAnchor,
-				constant: -ViewTraits.trailingMargin
+				lessThanOrEqualTo: disclosureView.leadingAnchor,
+				constant: -ViewTraits.Disclosure.trailingMargin
 			)
 		])
-
+		
 		setupDisclosureViewConstraints()
 	}
-
+	
 	func setupDisclosureViewConstraints() {
-
+		
 		NSLayoutConstraint.activate([
-
+			
 			disclosureView.trailingAnchor.constraint(
 				equalTo: trailingAnchor,
-				constant: -ViewTraits.disclosureMargin
+				constant: -ViewTraits.Disclosure.margin
 			),
-			disclosureView.heightAnchor.constraint(equalToConstant: ViewTraits.disclosureHeight),
+			disclosureView.heightAnchor.constraint(equalToConstant: ViewTraits.Disclosure.height),
+			disclosureView.widthAnchor.constraint(equalToConstant: ViewTraits.Disclosure.width),
 			disclosureView.centerYAnchor.constraint(equalTo: centerYAnchor)
 		])
 	}
-
+	
 	/// Setup all the accessibility traits
 	override func setupAccessibility() {
-
+		
 		super.setupAccessibility()
 		
 		accessibilityElements = [button]
 	}
-
+	
 	func setAccessibilityLabel() {
-
+		
 		button.accessibilityLabel = "\(title ?? ""). \(subtitle ?? "")"
 	}
-
+	
 	/// User tapped on the primary button
 	@objc func primaryButtonTapped() {
-
+		
 		primaryButtonTappedCommand?()
 	}
-
+	
 	// MARK: Public Access
-
+	
 	/// The user tapped on the primary button
 	var primaryButtonTappedCommand: (() -> Void)?
-
+	
 	/// The  title
 	var title: String? {
 		didSet {
-			titleLabel.attributedText = title?.setLineHeight(ViewTraits.Title.lineHeight,
-															 kerning: ViewTraits.Title.kerning)
+			titleLabel.attributedText = title?.setLineHeight(
+				ViewTraits.Title.lineHeight,
+				kerning: ViewTraits.Title.kerning
+			)
 			setAccessibilityLabel()
 		}
 	}
-
+	
 	/// The sub title
 	var subtitle: String? {
 		didSet {
-			subtitleLabel.attributedText = subtitle?.setLineHeight(ViewTraits.Message.lineHeight,
-																   kerning: ViewTraits.Message.kerning)
+			subtitleLabel.attributedText = subtitle?.setLineHeight(
+				ViewTraits.Message.lineHeight,
+				kerning: ViewTraits.Message.kerning
+			)
 			setAccessibilityLabel()
 		}
 	}
