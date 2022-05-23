312c4182aef6a0f0d718d03c750f512f886957ec
Rool Paap <rool@rool.nl>
2022-05-23 10:42:34 +0200

    Fix indentation sha256

--- Sources/CTR/Infrastructure/Extensions/String+Data.swift
+++ Sources/CTR/Infrastructure/Extensions/String+Data.swift
@@ -9,32 +9,32 @@ import Foundation
 import CommonCrypto
 
 extension String {
-    
-    var sha256: String {
+	
+	var sha256: String {
 		
-        let str = cString(using: .utf8)
-        let strLen = CUnsignedInt(lengthOfBytes(using: .utf8))
-        let digestLen = Int(CC_SHA256_DIGEST_LENGTH)
-        let result = UnsafeMutablePointer<CUnsignedChar>.allocate(capacity: digestLen)
-
-        CC_SHA256(str, strLen, result)
-
-        let hash = NSMutableString()
-        
-        for index in 0 ..< digestLen {
-            hash.appendFormat("%02x", result[index])
-        }
-
-        result.deallocate()
-
-        return String(format: hash as String)
-    }
+		let str = cString(using: .utf8)
+		let strLen = CUnsignedInt(lengthOfBytes(using: .utf8))
+		let digestLen = Int(CC_SHA256_DIGEST_LENGTH)
+		let result = UnsafeMutablePointer<CUnsignedChar>.allocate(capacity: digestLen)
+		
+		CC_SHA256(str, strLen, result)
+		
+		let hash = NSMutableString()
+		
+		for index in 0 ..< digestLen {
+			hash.appendFormat("%02x", result[index])
+		}
+		
+		result.deallocate()
+		
+		return String(format: hash as String)
+	}
 }
 
 extension String {
-
+	
 	func base64Decoded() -> String? {
-
+		
 		var st = self
 		if self.count % 4 <= 2 {
 			st += String(repeating: "=", count: (self.count % 4))
@@ -45,18 +45,18 @@ extension String {
 }
 
 extension Data {
-    
-    var sha256: Data {
-
-        var hash = [UInt8](repeating: 0, count: Int(CC_SHA256_DIGEST_LENGTH))
-        
-        withUnsafeBytes {
-            _ = CC_SHA256($0.baseAddress, CC_LONG(count), &hash)
-        }
-        
-        return Data(hash)
-    }
-    
+	
+	var sha256: Data {
+		
+		var hash = [UInt8](repeating: 0, count: Int(CC_SHA256_DIGEST_LENGTH))
+		
+		withUnsafeBytes {
+			_ = CC_SHA256($0.baseAddress, CC_LONG(count), &hash)
+		}
+		
+		return Data(hash)
+	}
+	
 }
 
 public extension String {
