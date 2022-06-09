a48a782bde0049d7912575755b8dedcc02e6d51e
Rool Paap <rool@rool.nl>
2022-06-09 09:52:26 +0200

    4415: Fixed forgotten tests

--- CTRTests/Interface/Holder/ShowQR/Item/ShowQRViewItemControllerTests.swift
+++ CTRTests/Interface/Holder/ShowQR/Item/ShowQRViewItemControllerTests.swift
@@ -22,99 +22,102 @@ class ShowQRViewItemControllerTests: XCTestCase {
 	
 	// MARK: Test lifecycle
 
-//	override func setUpWithError() throws {
-//
-//		try super.setUpWithError()
-//		environmentSpies = setupEnvironmentSpies()
-//		delegateSpy = ShowQRItemViewModelDelegateSpy()
-//		screenCaptureDetector = ScreenCaptureDetectorSpy()
-//
-//		let greenCard = try XCTUnwrap(
-//			GreenCardModel.createFakeGreenCard(
-//				dataStoreManager: environmentSpies.dataStoreManager,
-//				type: .domestic,
-//				withValidCredential: true
-//			)
-//		)
-//
-//		viewModel = ShowQRItemViewModel(
-//			delegate: delegateSpy,
-//			greenCard: greenCard,
-//			disclosurePolicy: .policy3G,
-//			screenCaptureDetector: screenCaptureDetector
-//		)
-//		sut = ShowQRItemViewController(viewModel: viewModel)
-//		window = UIWindow()
-//	}
-//
-//	func loadView() {
-//
-//		window.addSubview(sut.view)
-//		RunLoop.current.run(until: Date())
-//	}
-//
-////	// MARK: - Tests
-//
-//	func test_content_domestic() throws {
-//
-//		// Given
-//		let greenCard = try XCTUnwrap(
-//			GreenCardModel.createFakeGreenCard(
-//				dataStoreManager: environmentSpies.dataStoreManager,
-//				type: .domestic,
-//				withValidCredential: true
-//			)
-//		)
-//		viewModel = ShowQRItemViewModel(
-//			delegate: delegateSpy,
-//			greenCard: greenCard,
-//			disclosurePolicy: .policy3G
-//		)
-//		sut = ShowQRItemViewController(viewModel: viewModel)
-//
-//		// When
-//		loadView()
-//
-//		// Then
-//		expect(self.sut.sceneView.accessibilityDescription) == L.holderShowqrDomesticQrTitle()
-//	}
-//
-//	/// Test the validity of the credential without credential
-//	func test_content_euGreenCard() throws {
-//
-//		// Given
-//		let greenCard = try XCTUnwrap(
-//			GreenCardModel.createFakeGreenCard(
-//				dataStoreManager: environmentSpies.dataStoreManager,
-//				type: .eu,
-//				withValidCredential: true
-//			)
-//		)
-//		viewModel = ShowQRItemViewModel(
-//			delegate: delegateSpy,
-//			greenCard: greenCard,
-//			disclosurePolicy: nil
-//		)
-//		sut = ShowQRItemViewController(viewModel: viewModel)
-//
-//		// When
-//		loadView()
-//
-//		// Then
-//		expect(self.sut.sceneView.accessibilityDescription) == L.holderShowqrEuQrTitle()
-//	}
-//
-//	/// Test the validity of the credential with valid credential while screencapturing
-//	func testValidityCredentialValidWithScreenCapture() {
-//
-//		// Given
-//		loadView()
-//		sut?.checkValidity()
-//
-//		// When
-//		screenCaptureDetector.invokedScreenCaptureDidChangeCallback?(true)
-//
-//		// Then
-//		expect(self.sut.sceneView.largeQRimageView.isHidden) == true
-//	}
+	override func setUpWithError() throws {
+
+		try super.setUpWithError()
+		environmentSpies = setupEnvironmentSpies()
+		delegateSpy = ShowQRItemViewModelDelegateSpy()
+		screenCaptureDetector = ScreenCaptureDetectorSpy()
+
+		let greenCard = try XCTUnwrap(
+			GreenCardModel.createFakeGreenCard(
+				dataStoreManager: environmentSpies.dataStoreManager,
+				type: .domestic,
+				withValidCredential: true
+			)
+		)
+
+		viewModel = ShowQRItemViewModel(
+			delegate: delegateSpy,
+			greenCard: greenCard,
+			disclosurePolicy: .policy3G,
+			state: .regular,
+			screenCaptureDetector: screenCaptureDetector
+		)
+		sut = ShowQRItemViewController(viewModel: viewModel)
+		window = UIWindow()
+	}
+
+	func loadView() {
+
+		window.addSubview(sut.view)
+		RunLoop.current.run(until: Date())
+	}
+
+	// MARK: - Tests
+
+	func test_content_domestic() throws {
+
+		// Given
+		let greenCard = try XCTUnwrap(
+			GreenCardModel.createFakeGreenCard(
+				dataStoreManager: environmentSpies.dataStoreManager,
+				type: .domestic,
+				withValidCredential: true
+			)
+		)
+		viewModel = ShowQRItemViewModel(
+			delegate: delegateSpy,
+			greenCard: greenCard,
+			disclosurePolicy: .policy3G,
+			state: .regular
+		)
+		sut = ShowQRItemViewController(viewModel: viewModel)
+
+		// When
+		loadView()
+
+		// Then
+		expect(self.sut.sceneView.accessibilityDescription) == L.holderShowqrDomesticQrTitle()
+	}
+
+	/// Test the validity of the credential without credential
+	func test_content_euGreenCard() throws {
+
+		// Given
+		let greenCard = try XCTUnwrap(
+			GreenCardModel.createFakeGreenCard(
+				dataStoreManager: environmentSpies.dataStoreManager,
+				type: .eu,
+				withValidCredential: true
+			)
+		)
+		viewModel = ShowQRItemViewModel(
+			delegate: delegateSpy,
+			greenCard: greenCard,
+			disclosurePolicy: nil,
+			state: .regular
+		)
+		sut = ShowQRItemViewController(viewModel: viewModel)
+
+		// When
+		loadView()
+
+		// Then
+		expect(self.sut.sceneView.accessibilityDescription) == L.holderShowqrEuQrTitle()
+	}
+
+	/// Test the validity of the credential with valid credential while screencapturing
+	func testValidityCredentialValidWithScreenCapture() {
+
+		// Given
+		loadView()
+		sut?.checkValidity()
+
+		// When
+		screenCaptureDetector.invokedScreenCaptureDidChangeCallback?(true)
+
+		// Then
+		expect(self.sut.sceneView.largeQRimageView.isHidden) == true
+	}
 }
