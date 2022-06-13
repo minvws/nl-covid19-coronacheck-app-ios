a7a92e943fa193ad1a96ee3fd90bc1af80e391a1
Ian Dundas <1131967+iandundas@users.noreply.github.com>
2022-06-13 12:50:45 +0200

    remove commented code

--- Sources/CTR/Interface/UpdatedDisclosurePolicy/UpdatedDisclosurePolicyCoordinator.swift
+++ Sources/CTR/Interface/UpdatedDisclosurePolicy/UpdatedDisclosurePolicyCoordinator.swift
@@ -55,7 +55,7 @@ class UpdatedDisclosurePolicyCoordinator: Coordinator {
 			),
 			allowsBackButton: multipaneMode,
 			allowsCloseButton: !multipaneMode,
-			allowsNextButton: true // multipaneMode
+			allowsNextButton: true
 		)
 		
 		let modalNavigationController = UINavigationController(rootViewController: viewController)
