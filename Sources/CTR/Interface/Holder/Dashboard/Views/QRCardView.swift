c71176e7bd8811b37dba5da3986a57c8f9659b07
Ian Dundas <1131967+iandundas@users.noreply.github.com>
2022-06-16 15:42:26 +0200

    Move ErrorDashboardCardView into QRCardView

--- Sources/CTR/Interface/Holder/Dashboard/Views/QRCardView.swift
+++ Sources/CTR/Interface/Holder/Dashboard/Views/QRCardView.swift
@@ -678,3 +678,91 @@ private final class DisclosurePolicyIndicatorView: BaseView {
 		}
 	}
 }
+
+private final class ErrorRowView: BaseView {
+
+	/// The display constants
+	private enum ViewTraits {
+
+		enum Color {
+			static let tint: UIColor = C.error()!
+		}
+		enum Font {
+			static let font: UIFont = Fonts.subhead
+			static let lineHeight: CGFloat = 20
+			static let kern: CGFloat = 0.25
+			static let paragraphSpacing: CGFloat = -3
+		}
+		enum Spacing {
+			static let iconToMessage: CGFloat = 8
+		}
+		enum Size {
+			static let icon: CGFloat = 12
+		}
+	}
+
+	private let iconImageView: UIImageView = {
+		let imageView = UIImageView(image: I.dashboard.error())
+		imageView.translatesAutoresizingMaskIntoConstraints = false
+		imageView.tintColor = ViewTraits.Color.tint
+		imageView.contentMode = .scaleAspectFit
+		imageView.accessibilityIdentifier = "ErrorRowView/iconImageView"
+		return imageView
+	}()
+
+	let messageTextView: TextView = {
+		let view = TextView()
+		view.translatesAutoresizingMaskIntoConstraints = false
+		view.setContentCompressionResistancePriority(.required, for: .vertical)
+		view.accessibilityIdentifier = "ErrorRowView/messageTextView"
+		return view
+	}()
+
+	override func setupViewHierarchy() {
+		super.setupViewHierarchy()
+
+		addSubview(iconImageView)
+		addSubview(messageTextView)
+	}
+
+	override func setupViewConstraints() {
+		super.setupViewConstraints()
+
+		let iconOffset = ViewTraits.Font.lineHeight - ViewTraits.Font.font.ascender
+
+		NSLayoutConstraint.activate([
+			iconImageView.leadingAnchor.constraint(equalTo: leadingAnchor),
+			iconImageView.widthAnchor.constraint(equalToConstant: ViewTraits.Size.icon),
+			iconImageView.heightAnchor.constraint(equalToConstant: ViewTraits.Size.icon),
+
+			iconImageView.centerYAnchor.constraint(equalTo: messageTextView.centerYAnchor),
+
+			messageTextView.leadingAnchor.constraint(equalTo: iconImageView.trailingAnchor, constant: ViewTraits.Spacing.iconToMessage),
+			messageTextView.topAnchor.constraint(equalTo: topAnchor),
+			messageTextView.trailingAnchor.constraint(equalTo: trailingAnchor),
+			messageTextView.bottomAnchor.constraint(equalTo: bottomAnchor)
+		])
+	}
+
+	var message: String? {
+		didSet {
+
+			NSAttributedString.makeFromHtml(
+				text: message,
+				style: NSAttributedString.HTMLStyle(
+					font: Fonts.subhead,
+					textColor: ViewTraits.Color.tint,
+					lineHeight: ViewTraits.Font.lineHeight,
+					kern: ViewTraits.Font.kern,
+					paragraphSpacing: ViewTraits.Font.paragraphSpacing
+				)) {
+					self.messageTextView.attributedText = $0
+					self.setNeedsLayout()
+					self.layoutIfNeeded()
+				}
+
+			// Apply link color
+			messageTextView.linkTextAttributes = [.foregroundColor: ViewTraits.Color.tint]
+		}
+	}
+}
