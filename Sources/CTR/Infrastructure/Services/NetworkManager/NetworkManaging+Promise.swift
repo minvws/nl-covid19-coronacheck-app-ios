ab1df96e2f97db49bc59ed71493064bd8a1ddd8f
Ian Dundas <1131967+iandundas@users.noreply.github.com>
2022-08-24 15:34:19 +0200

    Add Promise shims to NetworkManaging

---
+++ Sources/CTR/Infrastructure/Services/NetworkManager/NetworkManaging+Promise.swift
@@ -0,0 +1,26 @@
+//
+/*
+* Copyright (c) 2022 De Staat der Nederlanden, Ministerie van Volksgezondheid, Welzijn en Sport.
+*  Licensed under the EUROPEAN UNION PUBLIC LICENCE v. 1.2
+*
+*  SPDX-License-Identifier: EUPL-1.2
+*/
+
+import Foundation
+
+extension NetworkManaging {
+	
+	/// Shim to allow return of Promise for `prepareIssue`
+	func prepareIssue() -> Promise<PrepareIssueEnvelope> {
+		Promise(work: { [self] fulfill, reject in
+			prepareIssue(completion: resultCompletionHandler(fulfil: fulfill, reject: reject))
+		})
+	}
+	
+	/// Shim to allow return of Promise for `fetchGreencards`
+	func fetchGreencards(dictionary: [String : AnyObject]) -> Promise<RemoteGreenCards.Response> {
+		Promise(work: { [self] fulfill, reject in
+			fetchGreencards(dictionary: dictionary, completion: resultCompletionHandler(fulfil: fulfill, reject: reject))
+		})
+	}
+}
