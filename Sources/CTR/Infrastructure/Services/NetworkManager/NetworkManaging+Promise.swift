d6ad47571786d6c857acfda36cbfb8fc5c273615
Ian Dundas <1131967+iandundas@users.noreply.github.com>
2022-08-24 16:33:12 +0200

    Lints

--- Sources/CTR/Infrastructure/Services/NetworkManager/NetworkManaging+Promise.swift
+++ Sources/CTR/Infrastructure/Services/NetworkManager/NetworkManaging+Promise.swift
@@ -1,4 +1,3 @@
-//
 /*
 * Copyright (c) 2022 De Staat der Nederlanden, Ministerie van Volksgezondheid, Welzijn en Sport.
 *  Licensed under the EUROPEAN UNION PUBLIC LICENCE v. 1.2
--- Sources/CTR/Infrastructure/Utilities/Logging.swift
+++ Sources/CTR/Infrastructure/Utilities/Logging.swift
@@ -136,4 +136,3 @@ extension Promise {
 		}
 	}
 }
-
