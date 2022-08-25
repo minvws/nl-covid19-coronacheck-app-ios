ea30c7b8fe6c14036f65587d8b90ccf98c7c9aeb
Ian Dundas <1131967+iandundas@users.noreply.github.com>
2022-08-25 16:31:28 +0200

    Logging on Future type

--- Sources/CTR/Infrastructure/Utilities/Logging.swift
+++ Sources/CTR/Infrastructure/Utilities/Logging.swift
@@ -8,6 +8,7 @@
 // swiftlint:disable disable_print
 
 import Foundation
+import BrightFutures
 
 private enum LoggingLevel: String, Comparable {
 	
@@ -98,40 +99,40 @@ private func log(icon: String, message: String, _ values: Any...) {
 	}
 }
 
-//extension Promise {
-//	
-//	func logVerbose(_ message: String) -> Promise<Value> {
-//		return self.then { value -> Value in
-//			CTR.logVerbose(message + ": \(value)")
-//			return value
-//		}
-//	}
-//	
-//	func logDebug(_ message: String) -> Promise<Value> {
-//		return self.then { value -> Value in
-//			CTR.logDebug(message + ": \(value)")
-//			return value
-//		}
-//	}
-//	
-//	func logInfo(_ message: String)  -> Promise<Value> {
-//		return self.then { value -> Value in
-//			CTR.logInfo(message + ": \(value)")
-//			return value
-//		}
-//	}
-//	
-//	func logWarning(_ message: String)  -> Promise<Value> {
-//		return self.then { value -> Value in
-//			CTR.logWarning(message + ": \(value)")
-//			return value
-//		}
-//	}
-//	
-//	func logError(_ message: String)  -> Promise<Value> {
-//		return self.then { value -> Value in
-//			CTR.logError(message + ": \(value)")
-//			return value
-//		}
-//	}
-//}
+extension Future {
+	
+	func logVerbose(_ message: String) -> Future<T, E> {
+		return self.map { value -> T in
+			CTR.logVerbose(message + ": \(value)")
+			return value
+		}
+	}
+	
+	func logDebug(_ message: String) -> Future<T, E> {
+		return self.map { value -> T in
+			CTR.logDebug(message + ": \(value)")
+			return value
+		}
+	}
+	
+	func logInfo(_ message: String) -> Future<T, E> {
+		return self.map { value -> T in
+			CTR.logInfo(message + ": \(value)")
+			return value
+		}
+	}
+	
+	func logWarning(_ message: String) -> Future<T, E> {
+		return self.map { value -> T in
+			CTR.logWarning(message + ": \(value)")
+			return value
+		}
+	}
+	
+	func logError(_ message: String) -> Future<T, E> {
+		return self.map { value -> T in
+			CTR.logError(message + ": \(value)")
+			return value
+		}
+	}
+}
