8470ed1aaf038ed4df9821352c33a3a60f0bdf96
Rool Paap <rool@rool.nl>
2022-10-11 13:33:38 +0200

    4821: Test for details scene

---
+++ CTRTests/Interface/Holder/FuzzyMatching/Details/DetailsIdentitySelectionViewControllerTests.swift
@@ -0,0 +1,48 @@
+/*
+* Copyright (c) 2022 De Staat der Nederlanden, Ministerie van Volksgezondheid, Welzijn en Sport.
+*  Licensed under the EUROPEAN UNION PUBLIC LICENCE v. 1.2
+*
+*  SPDX-License-Identifier: EUPL-1.2
+*/
+
+import XCTest
+import Nimble
+import SnapshotTesting
+@testable import CTR
+
+// swiftlint:disable type_name
+final class DetailsIdentitySelectionViewControllerTests: XCTestCase {
+	
+	var sut: IdentitySelectionDetailsViewController!
+	var window = UIWindow()
+	
+	override func setUp() {
+		super.setUp()
+		
+		sut = IdentitySelectionDetailsViewController(
+			viewModel: IdentitySelectionDetailsViewModel(
+				identitySelectionDetails:
+					IdentitySelectionDetails(
+						name: "Test",
+						details: [
+							["Vaccination", "Fetched from GGD", "Today"],
+							["Negative Test", "Fetched from TEST BOER BV", "Yesterday"]
+						]
+					)
+			)
+		)
+		window = UIWindow()
+	}
+	
+	func loadView() {
+
+		window.addSubview(sut.view)
+		RunLoop.current.run(until: Date())
+	}
+	
+	func test_snapshot() {
+		
+		loadView()
+		sut.assertImage()
+	}
+}
---
+++ CTRTests/Interface/Holder/FuzzyMatching/Details/DetailsIdentitySelectionViewModelTests.swift
@@ -0,0 +1,34 @@
+/*
+* Copyright (c) 2022 De Staat der Nederlanden, Ministerie van Volksgezondheid, Welzijn en Sport.
+*  Licensed under the EUROPEAN UNION PUBLIC LICENCE v. 1.2
+*
+*  SPDX-License-Identifier: EUPL-1.2
+*/
+
+import XCTest
+import Nimble
+@testable import CTR
+
+final class DetailsIdentitySelectionViewModelTests: XCTestCase {
+
+	var sut: IdentitySelectionDetailsViewModel!
+
+	func test_init() {
+		
+		// Given
+		let details = IdentitySelectionDetails(
+			name: "Test",
+			details: [["Vaccination", "Today"], ["Negative Test", "Yesterday"]]
+		)
+		
+		// When
+		sut = IdentitySelectionDetailsViewModel(identitySelectionDetails: details)
+		
+		// Then
+		expect(self.sut.title.value) == L.general_details()
+		expect(self.sut.message.value) == L.holder_identitySelection_details_body("Test")
+		expect(self.sut.details.value).to(haveCount(2))
+		expect(self.sut.details.value.first).to(haveCount(2))
+		expect(self.sut.details.value.last).to(haveCount(2))
+	}
+}
