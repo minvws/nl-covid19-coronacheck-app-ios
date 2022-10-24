8712de1a1b2eeed24ca072503dc464d69636662f
Rool Paap <rool@rool.nl>
2022-10-24 09:10:02 +0200

    4973: Updated Identity Check with birthdate only check.

--- Sources/CTR/Infrastructure/Services/IdentityChecker.swift
+++ Sources/CTR/Infrastructure/Services/IdentityChecker.swift
@@ -29,32 +29,16 @@ class IdentityChecker: IdentityCheckerProtocol {
 	func compare(eventGroups: [EventGroup], with remoteEvents: [RemoteEvent]) -> Bool {
 
 		var match = true
-		let existingIdentities: [Any] = convertEventGroupsToIdentities(eventGroups)
-		let remoteIdentities: [Any] = convertRemoteEventsToIdentities(remoteEvents)
+		let existingIdentities: [EventFlow.Identity] = convertEventGroupsToIdentities(eventGroups)
+		let remoteIdentities: [EventFlow.Identity] = convertRemoteEventsToIdentities(remoteEvents)
 
 		for existingIdentity in existingIdentities {
-
-			var existingTuple: IdentityTuple?
-
-			if let existing = existingIdentity as? EventFlow.Identity {
-				existingTuple = existing.asIdentityTuple()
-			}
-			logVerbose("existingIdentity: \(String(describing: existingTuple))")
-
 			for remoteIdentity in remoteIdentities {
-
-				var remoteTuple: IdentityTuple?
-
-				if let remote = remoteIdentity as? EventFlow.Identity {
-					remoteTuple = remote.asIdentityTuple()
-				}
-				logVerbose("remoteIdentity: \(String(describing: remoteTuple))")
-
-				match = match &&
-					remoteTuple?.day == existingTuple?.day &&
-					remoteTuple?.month == existingTuple?.month &&
-					(isInitialEqual(remoteTuple?.firstNameInitial, existingTuple?.firstNameInitial) ||
-						isInitialEqual(remoteTuple?.lastNameInitial, existingTuple?.lastNameInitial))
+				// US 4973: Only check if the date of birth is equal
+				match = match
+				&& remoteIdentity.getBirthDay() == existingIdentity.getBirthDay()
+				&& remoteIdentity.getBirthMonth() == existingIdentity.getBirthMonth()
+				&& remoteIdentity.getBirthYear() == existingIdentity.getBirthYear()
 			}
 		}
 		if !match {
@@ -63,19 +47,9 @@ class IdentityChecker: IdentityCheckerProtocol {
 		return match
 	}
 
-	private func isInitialEqual(_ lhs: String?, _ rhs: String?) -> Bool {
+	private func convertEventGroupsToIdentities(_ eventGroups: [EventGroup]) -> [EventFlow.Identity] {
 
-		switch (lhs, rhs) {
-			case (nil, _), (_, nil):
-				return true
-			default:
-				return lhs == rhs
-		}
-	}
-
-	private func convertEventGroupsToIdentities(_ eventGroups: [EventGroup]) -> [Any] {
-
-		var identities = [Any]()
+		var identities = [EventFlow.Identity]()
 
 		for storedEvent in eventGroups {
 			if let jsonData = storedEvent.jsonData {
@@ -95,7 +69,7 @@ class IdentityChecker: IdentityCheckerProtocol {
 		return identities
 	}
 
-	private func convertRemoteEventsToIdentities(_ remoteEvents: [RemoteEvent]) -> [Any] {
+	private func convertRemoteEventsToIdentities(_ remoteEvents: [RemoteEvent]) -> [EventFlow.Identity] {
 
 		return remoteEvents.compactMap {
 			return $0.wrapper.identity
@@ -103,31 +77,9 @@ class IdentityChecker: IdentityCheckerProtocol {
 	}
 }
 
-typealias IdentityTuple = (firstNameInitial: String?, lastNameInitial: String?, day: String?, month: String?)
-
 extension EventFlow.Identity {
 
-	func asIdentityTuple() -> IdentityTuple {
-
-		return (
-			firstNameInitial: getFirstNameInitIal(),
-			lastNameInitial: getLastNameInitial(),
-			day: getBirthDay(),
-			month: getBirthMonth()
-		)
-	}
-
-	private func getFirstNameInitIal() -> String? {
-
-		return Normalizer.toAzInitial(firstName)?.uppercased()
-	}
-
-	private func getLastNameInitial() -> String? {
-
-		return Normalizer.toAzInitial(lastName)?.uppercased()
-	}
-
-	private func getBirthDay() -> String? {
+	public func getBirthDay() -> String? {
 
 		guard let birthDate = birthDateString.flatMap(Formatter.getDateFrom) else {
 			return nil
@@ -139,7 +91,7 @@ extension EventFlow.Identity {
 		return nil
 	}
 
-	private func getBirthMonth() -> String? {
+	public func getBirthMonth() -> String? {
 
 		guard let birthDate = birthDateString.flatMap(Formatter.getDateFrom) else {
 			return nil
@@ -150,42 +102,16 @@ extension EventFlow.Identity {
 		}
 		return nil
 	}
-}
+	
+	public func getBirthYear() -> String? {
 
-class Normalizer {
-
-	static let permittedCharacterSet = CharacterSet(charactersIn: "abcdefghijklmnopqrstuvwxyz ")
-	static let initialsCharacterSet = CharacterSet(charactersIn: "ABCDEFGHIJKLMNOPQRSTUVWXYZ")
-	static let filterCharacterSet = CharacterSet(charactersIn: "-' ")
-
-	/// Normalize any input, transform to latin, remove all diacritics
-	/// - Parameter input: the unnormalized input
-	/// - Returns: normalized output
-	class func normalize(_ input: String) -> String? {
-
-		if let latinInput = input.applyingTransform(StringTransform("Any-Latin; Latin-ASCII; Lower;"), reverse: false) {
-			let permittedInput = String(latinInput.unicodeScalars.filter { permittedCharacterSet.contains($0) })
-			return permittedInput
+		guard let birthDate = birthDateString.flatMap(Formatter.getDateFrom) else {
+			return nil
+		}
+		let components = Calendar.current.dateComponents([.year], from: birthDate)
+		if let yearInt = components.year {
+			return "\(yearInt)"
 		}
 		return nil
-	}
-
-	/// Return the initial of the input, only if is in A-Z
-	/// - Parameter input: the string to check
-	/// - Returns: optional the initial
-	class func toAzInitial(_ input: String?) -> String? {
-
-		guard let input = input, !input.isEmpty else {
-			return nil
-		}
-
-		let validInput = String(input.unicodeScalars.filter { !filterCharacterSet.contains($0) })
-		let firstChar = validInput.prefix(1)
-		let capitalizedInitial = String(firstChar).uppercased()
-		guard capitalizedInitial.unicodeScalars.allSatisfy({ initialsCharacterSet.contains($0) }) else {
-
-			return nil
-		}
-		return capitalizedInitial
 	}
 }
