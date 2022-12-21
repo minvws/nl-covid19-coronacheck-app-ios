f714bc0304123746b1d48616d3ba9bf5767ffa68
Rool Paap <rool@rool.nl>
2022-12-21 09:46:12 +0100

    More unit tests for Collection and Result

--- Packages/Shared/Tests/SharedTests/SharedTests.swift
+++ Packages/Shared/Tests/SharedTests/SharedTests.swift
@@ -2,7 +2,9 @@ import XCTest
 @testable import Shared
 
 final class SharedTests: XCTestCase {
-
+	
+	// MARK: - fullStringVersion (String + Version)
+	
 	func test_fullStringVersion_singleDigit() {
 		
 		// Given
@@ -25,5 +27,86 @@ final class SharedTests: XCTestCase {
 		
 		// Then
 		XCTAssertEqual(result, "2.1.0")
+	}
+	
+	// MARK: - Collection
+	
+	func test_array_isNotEmpty() {
+		
+		// Given
+		let values = ["🦠"]
+		
+		// Then
+		XCTAssertTrue(values.isNotEmpty)
+		XCTAssertFalse(values.isEmpty)
+	}
+	
+	func test_array_isEmpty() {
+		
+		// Given
+		let values: [String] = []
+		
+		// Then
+		XCTAssertFalse(values.isNotEmpty)
+		XCTAssertTrue(values.isEmpty)
+	}
+	
+	func test_string_isNotEmpty() {
+		
+		// Given
+		let value = ["🦠"]
+		
+		// Then
+		XCTAssertTrue(value.isNotEmpty)
+		XCTAssertFalse(value.isEmpty)
+	}
+	
+	func test_string_isEmpty() {
+		
+		// Given
+		let value = ""
+		
+		// Then
+		XCTAssertFalse(value.isNotEmpty)
+		XCTAssertTrue(value.isEmpty)
+	}
+	
+	// MARK: - Result
+	
+	func test_isSuccess() {
+		
+		// Given
+		let result: Result<String, Error> = .success("Test")
+		
+		// When
+		let isSuccess = result.isSuccess
+		let isFailure = result.isFailure
+		let successValue = result.successValue
+		let failureErrror = result.failureError
+		
+		// Then
+		XCTAssertTrue(isSuccess)
+		XCTAssertFalse(isFailure)
+		XCTAssertEqual(successValue, "Test")
+		XCTAssertNil(failureErrror)
+	}
+	
+	func test_isFailure() {
+		
+		// Given
+		let error = NSError(domain: NSURLErrorDomain, code: URLError.notConnectedToInternet.rawValue)
+		let result: Result<String, Error> = .failure(error)
+		
+		// When
+		let isSuccess = result.isSuccess
+		let isFailure = result.isFailure
+		let successValue = result.successValue
+		let failureErrror = result.failureError
+		
+		// Then
+		XCTAssertFalse(isSuccess)
+		XCTAssertTrue(isFailure)
+		XCTAssertNil(successValue)
+		XCTAssertNotNil(failureErrror)
 	}
 }
