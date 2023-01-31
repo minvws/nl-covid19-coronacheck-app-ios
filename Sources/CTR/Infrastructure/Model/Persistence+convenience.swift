24982aee87cd461f3982dca3d5a6c564f6312709
Ian Dundas <1131967+iandundas@users.noreply.github.com>
2023-01-31 13:19:38 +0100

    fix mistaken find/replace

--- Sources/CTR/Infrastructure/Model/Persistence+convenience.swift
+++ Sources/CTR/Infrastructure/Model/Persistence+convenience.swift
@@ -1,6 +1,6 @@
 /*
 * Copyright (c) 2022 De Staat der Nederlanden, Ministerie van Volksgezondheid, Welzijn en Sport.
-*  Licensed under the EUROPEAN UNION LICENCE v. 1.2
+*  Licensed under the EUROPEAN UNION PUBLIC LICENCE v. 1.2
 *
 *  SPDX-License-Identifier: EUPL-1.2
 */
