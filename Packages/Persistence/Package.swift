83ce40eab63de8523db3e8f0cc80553d6a805cce
Ian Dundas <1131967+iandundas@users.noreply.github.com>
2023-01-31 11:49:45 +0100

    Spaces are better but tabs are everywhere

--- Packages/Persistence/Package.swift
+++ Packages/Persistence/Package.swift
@@ -4,28 +4,28 @@
 import PackageDescription
 
 let package = Package(
-    name: "Persistence",
-    products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "Persistence",
-            targets: ["Persistence"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-        .package(name: "Shared", path: "../Shared"),
-
-        // testing:
+	name: "Persistence",
+	products: [
+		// Products define the executables and libraries a package produces, and make them visible to other packages.
+		.library(
+			name: "Persistence",
+			targets: ["Persistence"]),
+	],
+	dependencies: [
+		// Dependencies declare other packages that this package depends on.
+		// .package(url: /* package url */, from: "1.0.0"),
+		.package(name: "Shared", path: "../Shared"),
+		
+		// testing:
 		.package(name: "TestingShared", path: "../TestingShared"),
 		.package(url: "https://github.com/Quick/Nimble", from: .init(10, 0, 0))
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "Persistence",
-            dependencies: [
+	],
+	targets: [
+		// Targets are the basic building blocks of a package. A target can define a module or a test suite.
+		// Targets can depend on other targets in this package, and on products in packages this package depends on.
+		.target(
+			name: "Persistence",
+			dependencies: [
 				.product(name: "Shared", package: "Shared"),
 			],
 			resources: [
@@ -33,12 +33,12 @@ let package = Package(
 				.copy("Schemas/Verifier.xcdatamodeld"),
 			]
 		),
-        .testTarget(
-            name: "PersistenceTests",
-            dependencies: [
-                "Persistence",
-                .product(name: "TestingShared", package: "TestingShared"),
+		.testTarget(
+			name: "PersistenceTests",
+			dependencies: [
+				"Persistence",
+				.product(name: "TestingShared", package: "TestingShared"),
 				.product(name: "Nimble", package: "Nimble"),
-            ]),
-    ]
+			]),
+	]
 )
