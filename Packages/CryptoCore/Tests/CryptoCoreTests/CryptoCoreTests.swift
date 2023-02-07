e67ab7ff6c6998a3686490d34a35f904e12c2ece
Ian Dundas <1131967+iandundas@users.noreply.github.com>
2023-02-07 11:39:14 +0100

    Added a CryptoCore package

---
+++ Packages/CryptoCore/Tests/CryptoCoreTests/CryptoCoreTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import CryptoCore
+
+final class CryptoCoreTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(CryptoCore().text, "Hello, World!")
+    }
+}
