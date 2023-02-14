96662309737089ec6fefaa0467f551dc97da7c6c
Ian Dundas <1131967+iandundas@users.noreply.github.com>
2023-02-14 14:49:26 +0100

    Replace incorrect file headers

--- CTRTests/Fakes/PersistenceFakes.swift
+++ CTRTests/Fakes/PersistenceFakes.swift
@@ -1,9 +1,9 @@
-//
-//  File.swift
-//  
-//
-//  Created by Ian Dundas on 14/02/2023.
-//
+/*
+* Copyright (c) 2022 De Staat der Nederlanden, Ministerie van Volksgezondheid, Welzijn en Sport.
+*  Licensed under the EUROPEAN UNION PUBLIC LICENCE v. 1.2
+*
+*  SPDX-License-Identifier: EUPL-1.2
+*/
 
 import Foundation
 @testable import Persistence
