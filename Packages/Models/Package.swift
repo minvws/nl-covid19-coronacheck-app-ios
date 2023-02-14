a72eb1fcaca30d32a78cef1ed260879a27867ece
Ian Dundas <1131967+iandundas@users.noreply.github.com>
2023-02-14 13:04:04 +0100

    Models depends on Persistence and Transport

--- Packages/Models/Package.swift
+++ Packages/Models/Package.swift
@@ -13,8 +13,10 @@ let package = Package(
 	],
 	dependencies: [
 		.package(name: "CryptoCore", path: "../CryptoCore"),
-		.package(name: "Shared", path: "../Shared"),
-		
+		.package(name: "Persistence", path: "../Persistence"),
+        .package(name: "Shared", path: "../Shared"),
+		.package(name: "Transport", path: "../Transport"),
+
 		.package(url: "https://github.com/minvws/nl-rdo-app-ios-modules", branch: "main"),
 		
 		// Testing:
@@ -27,8 +29,11 @@ let package = Package(
 		.target(
 			name: "Models",
 			dependencies: [
-				.product(name: "CryptoCore", package: "CryptoCore"),
-				.product(name: "Shared", package: "Shared"),
+                .product(name: "CryptoCore", package: "CryptoCore"),
+				.product(name: "Persistence", package: "Persistence"),
+                .product(name: "Shared", package: "Shared"),
+				.product(name: "Transport", package: "Transport"),
+				
 				.product(name: "LuhnCheck", package: "nl-rdo-app-ios-modules"),
 			]),
 		.testTarget(
