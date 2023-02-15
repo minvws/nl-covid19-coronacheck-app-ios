2190f31a0d8a169be8336ef796e8ac4319c38fa4
Ian Dundas <1131967+iandundas@users.noreply.github.com>
2023-02-15 10:56:29 +0100

    Lint

--- Packages/Resources/Package.swift
+++ Packages/Resources/Package.swift
@@ -4,44 +4,44 @@
 import PackageDescription
 
 let package = Package(
-    name: "Resources",
-    defaultLocalization: "nl",
-    products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "Resources",
-            targets: ["Resources"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        .package(name: "Shared", path: "../Shared"),
+	name: "Resources",
+	defaultLocalization: "nl",
+	products: [
+		// Products define the executables and libraries a package produces, and make them visible to other packages.
+		.library(
+			name: "Resources",
+			targets: ["Resources"]),
+	],
+	dependencies: [
+		// Dependencies declare other packages that this package depends on.
+		.package(name: "Shared", path: "../Shared"),
 		.package(url: "https://github.com/mac-cain13/R.swift.git", from: "7.0.0"),
-
-        // testing:
+		
+		// testing:
 		.package(name: "TestingShared", path: "../TestingShared"),
 		.package(url: "https://github.com/Quick/Nimble", from: .init(10, 0, 0))
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "Resources",
-            dependencies: [
-                .product(name: "Shared", package: "Shared"),
-                .product(name: "RswiftLibrary", package: "R.swift"),
-            ],
-          	resources: 	[
+	],
+	targets: [
+		// Targets are the basic building blocks of a package. A target can define a module or a test suite.
+		// Targets can depend on other targets in this package, and on products in packages this package depends on.
+		.target(
+			name: "Resources",
+			dependencies: [
+				.product(name: "Shared", package: "Shared"),
+				.product(name: "RswiftLibrary", package: "R.swift"),
+			],
+			resources: 	[
 				.copy("Resources/Animations"),
 				.process("Fonts")
 			],
 			plugins: [.plugin(name: "RswiftGeneratePublicResources", package: "R.swift")]
-        ),
-        .testTarget(
-            name: "ResourcesTests",
-            dependencies: [
-                "Resources",
-                .product(name: "TestingShared", package: "TestingShared"),
+		),
+		.testTarget(
+			name: "ResourcesTests",
+			dependencies: [
+				"Resources",
+				.product(name: "TestingShared", package: "TestingShared"),
 				.product(name: "Nimble", package: "Nimble"),
-            ]),
-    ]
+			]),
+	]
 )
