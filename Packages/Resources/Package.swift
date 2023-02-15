811b123a7071ccab7835e11650a596a922eef06f
Ian Dundas <1131967+iandundas@users.noreply.github.com>
2023-02-15 10:42:34 +0100

    Move Strings+Colors+Fonts+Images into Resources package

--- Packages/Resources/Package.swift
+++ Packages/Resources/Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "Resources",
+    defaultLocalization: "nl",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -13,16 +14,34 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(name: "Shared", path: "../Shared"),
+		.package(url: "https://github.com/mac-cain13/R.swift.git", from: "7.0.0"),
+
+        // testing:
+		.package(name: "TestingShared", path: "../TestingShared"),
+		.package(url: "https://github.com/Quick/Nimble", from: .init(10, 0, 0))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Resources",
-            dependencies: []),
+            dependencies: [
+                .product(name: "Shared", package: "Shared"),
+                .product(name: "RswiftLibrary", package: "R.swift"),
+            ],
+          	resources: 	[
+				.copy("Resources/Animations"),
+				.process("Fonts")
+			],
+			plugins: [.plugin(name: "RswiftGeneratePublicResources", package: "R.swift")]
+        ),
         .testTarget(
             name: "ResourcesTests",
-            dependencies: ["Resources"]),
+            dependencies: [
+                "Resources",
+                .product(name: "TestingShared", package: "TestingShared"),
+				.product(name: "Nimble", package: "Nimble"),
+            ]),
     ]
 )
--- Packages/Resources/Sources/Resources/Resources.swift
+++
@@ -1,6 +0,0 @@
-public struct Resources {
-    public private(set) var text = "Hello, World!"
-
-    public init() {
-    }
-}
