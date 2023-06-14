c5615d1760fef09f3f2970c93702af166f67e46a
Rool Paap <rool@rool.nl>
2023-06-14 12:50:46 +0200

    5601: Renaming and moving files

--- Sources/CTR/Interface/Holder/PDF/Export/PrintAttributes.swift
+++ Sources/CTR/Interface/Holder/PDF/Export/PrintAttributes.swift
@@ -1,15 +1,29 @@
 /*
-* Copyright (c) 2022 De Staat der Nederlanden, Ministerie van Volksgezondheid, Welzijn en Sport.
-*  Licensed under the EUROPEAN UNION PUBLIC LICENCE v. 1.2
-*
-*  SPDX-License-Identifier: EUPL-1.2
-*/
+ * Copyright (c) 2022 De Staat der Nederlanden, Ministerie van Volksgezondheid, Welzijn en Sport.
+ *  Licensed under the EUROPEAN UNION PUBLIC LICENCE v. 1.2
+ *
+ *  SPDX-License-Identifier: EUPL-1.2
+ */
 
 import Foundation
 import Models
 
-struct EUPrintAttributes: Codable {
+struct PrintAttributes: Codable {
+	
+	public let european: [EUPrintAttributes]
+	
+	public enum CodingKeys: String, CodingKey {
+		
+		case european
+	}
+	
+	init(european: [EUPrintAttributes]) {
+		self.european = european
+	}
+}
 
+struct EUPrintAttributes: Codable {
+	
 	public let digitalCovidCertificate: EuCredentialAttributes.DigitalCovidCertificate
 	public let expirationTime: Date
 	public let qr: String
@@ -22,21 +36,9 @@ struct EUPrintAttributes: Codable {
 	}
 	
 	init(digitalCovidCertificate: EuCredentialAttributes.DigitalCovidCertificate, expirationTime: Date, qr: String) {
+		
 		self.digitalCovidCertificate = digitalCovidCertificate
 		self.expirationTime = expirationTime
 		self.qr = qr
-	}
-}
-
-struct PrintAttributes: Codable {
-	public let european: [EUPrintAttributes]
-	
-	public enum CodingKeys: String, CodingKey {
-		
-		case european
-	}
-	
-	init(european: [EUPrintAttributes]) {
-		self.european = european
 	}
 }
