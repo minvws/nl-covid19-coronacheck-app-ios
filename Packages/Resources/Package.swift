e4c8e3357e09b818e8c7efe675725d9929ec4c11
Rool Paap <rool@rool.nl>
2023-09-18 09:48:48 +0200

    Updated R.swift to 7.4.0

--- Packages/Resources/Package.swift
+++ Packages/Resources/Package.swift
@@ -17,7 +17,7 @@ let package = Package(
 		.package(name: "Shared", path: "../Shared"),
 		
 		// External:
-		.package(url: "https://github.com/mac-cain13/R.swift.git", from: "7.3.0"),
+		.package(url: "https://github.com/mac-cain13/R.swift.git", from: "7.4.0"),
 		
 		// Testing:
 		.package(name: "TestingShared", path: "../TestingShared"),
